c3ee60788f2cd390217fbcce3dde51c7c4f8e825
Clemens Wolff <clewolff@microsoft.com>
2017-04-27 09:23:10 -0700

    Introduce InstagramDStream

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 import java.util.concurrent.TimeUnit
 
-import com.microsoft.partnercatalyst.fortis.spark.sources.instagram.InstagramReceiver
+import com.microsoft.partnercatalyst.fortis.spark.sources.instagram.InstagramDStream
 import com.microsoft.partnercatalyst.fortis.spark.sources.instagram.client.{InstagramLocationClient, Location, Auth => InstagramAuth}
 import com.microsoft.partnercatalyst.fortis.spark.sources.Schedule
 import com.microsoft.partnercatalyst.fortis.spark.transforms.image.{ImageAnalyzer, Auth => VisionAuth}
@@ -13,11 +13,12 @@ object Main {
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc, Seconds(1))
 
-    val instagramStream = ssc.receiverStream(new InstagramReceiver(
+    val instagramStream = new InstagramDStream(
+      _ssc = ssc,
       schedule = Schedule(10, TimeUnit.SECONDS),
       client = new InstagramLocationClient(
         location = Location(lat = 49.25, lng = -123.1, radiusMeters = 5000),
-        auth = InstagramAuth("INSERT_INSTAGRAM_TOKEN_HERE"))))
+        auth = InstagramAuth("INSERT_INSTAGRAM_TOKEN_HERE")))
 
     val imageAnalysis = new ImageAnalyzer(
       auth = VisionAuth("INSERT_COGNITIVE_SERVICES_TOKEN_HERE"))
--- src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sources/instagram/InstagramReceiver.scala
+++ src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sources/instagram/InstagramReceiver.scala
@@ -4,6 +4,9 @@ import com.microsoft.partnercatalyst.fortis.spark.sources.instagram.client.Insta
 import com.microsoft.partnercatalyst.fortis.spark.sources.instagram.dto.Instagram
 import com.microsoft.partnercatalyst.fortis.spark.sources.{PollingReceiver, Schedule}
 import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.dstream.ReceiverInputDStream
+import org.apache.spark.streaming.receiver.Receiver
 
 class InstagramReceiver(
   client: InstagramClient,
@@ -22,5 +25,18 @@ class InstagramReceiver(
         store(x)
         if (x.createdAtEpoch > lastIngestedEpoch) lastIngestedEpoch = x.createdAtEpoch
       })
+  }
+}
+
+class InstagramDStream(
+  _ssc: StreamingContext,
+  client: InstagramClient,
+  schedule: Schedule,
+  storageLevel: StorageLevel = StorageLevel.MEMORY_ONLY,
+  numWorkers: Int = 1
+) extends ReceiverInputDStream[Instagram](_ssc) {
+
+  override def getReceiver(): Receiver[Instagram] = {
+    new InstagramReceiver(client, schedule, storageLevel, numWorkers)
   }
 }
