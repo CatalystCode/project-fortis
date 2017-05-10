085caceb2822f35f156503313fc44413dd1d449e
Clemens Wolff <clewolff@microsoft.com>
2017-05-10 16:34:16 -0700

    Don't run heavy tests all the time

--- src/test/scala/com/microsoft/partnercatalyst/fortis/spark/transforms/locations/nlp/PlaceRecognizerSpec.scala
+++ src/test/scala/com/microsoft/partnercatalyst/fortis/spark/transforms/locations/nlp/PlaceRecognizerSpec.scala
@@ -4,6 +4,10 @@ import org.scalatest.FlatSpec
 
 class PlaceRecognizerSpec extends FlatSpec {
   "The place recognizer" should "extract correct places" in {
+    if (Option(System.getenv("FORTIS_INTEGRATION_TESTS")).isEmpty) {
+      cancel("Integration tests disabled")
+    }
+
     val recognizer = new PlaceRecognizer()
     val places = recognizer.extractPlaces("I went to Paris last week. France was great!", "en")
     assert(places == Set("France", "Paris"))
