f66220a50c1948a4543e7b2f8195784ee68f4512
erikschlegel <marlene3>
2017-06-12 22:59:38 -0500

    Adding deis, spark and cassandra support for fortis deployment.

--- ops/create-cluster.sh
+++ ops/create-cluster.sh
@@ -5,7 +5,7 @@ git clone https://github.com/CatalystCode/charts.git
 cd charts
 
 ./install-cassandra.sh
-./install-spark
+./install-spark.sh
 #./install-postgis
 #./install-elasticsearch
 #./install-kibana
