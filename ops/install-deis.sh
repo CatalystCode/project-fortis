1552205882fe1278d855b0e686e29e06611b8e20
Clemens Wolff <clewolff@microsoft.com>
2017-06-17 21:22:44 +0200

    SC2155: avoid masking return value in export

--- ops/install-deis.sh
+++ ops/install-deis.sh
@@ -4,7 +4,8 @@ echo "creating deis storage account ${k8location}"
 DEIS_STORAGE_ACCOUNT_NAME=k8deisstorage
 
 sudo az storage account create -n "${DEIS_STORAGE_ACCOUNT_NAME}" -l "${k8location}" -g "${k8resource_group}" --sku Standard_LRS
-export DEIS_STORAGE_ACCOUNT_KEY="$(az storage account keys list -n "${DEIS_STORAGE_ACCOUNT_NAME}" -g "${k8resource_group}" --query [0].value --output tsv)"
+DEIS_STORAGE_ACCOUNT_KEY="$(az storage account keys list -n "${DEIS_STORAGE_ACCOUNT_NAME}" -g "${k8resource_group}" --query [0].value --output tsv)"
+export DEIS_STORAGE_ACCOUNT_KEY
 
 echo "starting deis installation using helm"
 sudo helm repo add deis https://charts.deis.com/workflow
