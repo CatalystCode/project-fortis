4f9dfeabbe5300f5843c25cbb3c25c16d11b6c35
erikschlegel <marlene3>
2017-06-17 23:12:04 -0500

    merging

--- ops/create-cluster.sh
+++ ops/create-cluster.sh
@@ -1,4 +1,4 @@
-chmod 752 *.sh
+sudo chmod 752 *.sh
 
 sudo ./create-disk.sh
 sudo ./install-deis.sh
@@ -7,6 +7,6 @@ git clone https://github.com/CatalystCode/charts.git
 
 sudo ./install-cassandra.sh
 sudo ./install-spark.sh
-sudo ./install-postgis
+#sudo ./install-postgis
 #./install-elasticsearch
 #./install-kibana
