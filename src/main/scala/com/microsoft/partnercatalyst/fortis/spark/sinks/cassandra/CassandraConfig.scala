4e30b7841bc63394df9a63440f7cf38e110ff7b7
Clemens Wolff <clewolff@microsoft.com>
2017-06-22 14:00:39 -0700

    Fix typo

--- src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sinks/cassandra/CassandraConfig.scala
+++ src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sinks/cassandra/CassandraConfig.scala
@@ -7,7 +7,7 @@ import org.apache.spark.streaming.Duration
 
 object CassandraConfig {
   def init(conf: SparkConf, batchDuration: Duration): SparkConf = {
-    conf.setIfMissing("spark.cassandra.connection.host", getenv("FORTIS_FEATURE_SERVICE_HOST"))
+    conf.setIfMissing("spark.cassandra.connection.host", getenv("FORTIS_CASSANDRA_HOST"))
         .setIfMissing("spark.cassandra.auth.username", getenv("FORTIS_CASSANDRA_USER"))
         .setIfMissing("spark.cassandra.auth.password", getenv("FORTIS_CASSANDRA_PASSWORD"))
         .setIfMissing("spark.cassandra.connection.keep_alive_ms", (batchDuration.milliseconds * 2).toString)
