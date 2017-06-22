c75e93f242e7b389f1001dffaa23860f2a5f525b
Clemens Wolff <clewolff@microsoft.com>
2017-06-22 12:49:26 -0700

    Integrate with test schema

--- src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sinks/cassandra/CassandraSchema.scala
+++ src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sinks/cassandra/CassandraSchema.scala
@@ -1,44 +1,16 @@
 package com.microsoft.partnercatalyst.fortis.spark.sinks.cassandra
 
 import com.microsoft.partnercatalyst.fortis.spark.dto.AnalyzedItem
-import net.liftweb.json
-import net.liftweb.json.JsonDSL._
-import net.liftweb.json.compactRender
 
 case class CassandraRow(
-  source: String,
-  title: String,
-  src_url: String,
-  detected_features: String,
-  publisher: String,
-  topics: String,
-  lang: String,
-  message_body: String,
-  feature_collection: String,
-  event_id: String)
+  created_at: Long,
+  pipeline: String)
 
 object CassandraSchema {
   def apply(item: AnalyzedItem): CassandraRow = {
-    implicit val formats = json.DefaultFormats
-
     CassandraRow(
-      source = "", // todo
-      title = "", // todo
-      src_url = item.source,
-      detected_features = compactRender(item.analysis.locations.map(_.wofId)),
-      publisher = "", // todo
-      topics = compactRender(item.analysis.keywords.map(_.name)),
-      lang = item.analysis.language.getOrElse(""),
-      message_body = "", // todo
-      feature_collection = toFeatureCollection(item), // todo
-      event_id = "" // todo
+      created_at = item.createdAtEpoch,
+      pipeline = item.publisher
     )
-  }
-
-  private def toFeatureCollection(item: AnalyzedItem): String = {
-    compactRender(
-      ("sentiments_avg" -> item.analysis.sentiments)
-      ~
-      ("entities" -> item.analysis.entities.map("name" -> _.name)))
   }
 }
