6bf337bd2bf0568118f78e001e1d6b506b772788
erikschlegel <marlene3>
2017-06-28 07:35:18 -0500

    Adding deployment for fortis interface site and pushing deployed service & graphql service hostnames to resource tags so they can be displayed to the user once the deployment finishes.

---
+++ ops/deis-apps/fortis-interface/create-app.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+fortis_interface_gh_repo=https://github.com/CatalystCode/project-fortis-interfaces
+
+git clone ${fortis_interface_gh_repo}
+
+deis create fortis-interface
--- ops/setup-environment.sh
+++ ops/setup-environment.sh
@@ -7,14 +7,23 @@ feature_service_host="$4"
 SPARK_DAEMON_MEMORY="1g"
 spark_config_map_name="$5"
 graphql_service_host="$6"
+k8resource_group="$7"
+fortis_interface_host="$8"
 default_language="en"
 checkpoint_directory="HDFS://"
+
 fortis_models_directory="https://fortiscentral.blob.core.windows.net/sentiment/"
+fortis_central_directory="https://fortiscentral.blob.core.windows.net/"
 
 kubectl create configmap "${spark_config_map_name}" --namespace spark --from-literal=FORTIS_CASSANDRA_HOST="${cassandra_host}" --from-literal=FORTIS_FEATURE_SERVICE_HOST="${feature_service_host}" --from-literal=DEFAULT_SITE_NAME="${site_name}" --from-literal=FORTIS_APPINSIGHTS_IKEY="${app_insights_id}" --from-literal=SPARK_DAEMON_MEMORY="${SPARK_DAEMON_MEMORY}" --from-literal=HA_PROGRESS_DIR="${checkpoint_directory}" --from-literal=DEFAULT_LANGUAGE="${default_language}" --from-literal=FORTIS_SERVICE_HOST="${graphql_service_host}" --from-literal=FORTIS_MODELS_DIRECTORY="${fortis_models_directory}"
 
 deis config:set APPINSIGHTS_INSTRUMENTATIONKEY="${app_insights_id}"
 deis config:set FEATURE_SERVICE_HOST="${feature_service_host}"
-deis config:set CASSANDRA_SERVICE_HOST="${cassandra_host}"
+deis config:set CASSANDRA_CONTACT_POINTS="${cassandra_host}"
 deis config:set DEFAULT_SITE_NAME="${site_name}"
 deis config:set FORTIS_SERVICE_HOST="${graphql_service_host}"
+deis config:set FORTIS_CENTRAL_ASSETS_HOST="${fortis_central_directory}"
+
+#Set the deployed service host url tag so we can output that on the deployment console to the user
+az resource tag --tags FORTIS_SERVICE_HOST="${graphql_service_host}" -g "${k8resource_group}" -n k8deisstorage --resource-type "Microsoft.Storage/storageAccounts"
+az resource tag --tags FORTIS_INTERFACE_HOST="${fortis_interface_host}" -g "${k8resource_group}" -n k8deisstorage --resource-type "Microsoft.Storage/storageAccounts"
