0cef163156e3699ed15b233406063fcd95810685
Clemens Wolff <clewolff@microsoft.com>
2017-06-28 11:31:29 -0700

    Remove redundant comment

--- src/main/scala/com/microsoft/partnercatalyst/fortis/spark/streamfactories/TwitterStreamFactory.scala
+++ src/main/scala/com/microsoft/partnercatalyst/fortis/spark/streamfactories/TwitterStreamFactory.scala
@@ -9,13 +9,6 @@ import twitter4j.auth.OAuthAuthorization
 import twitter4j.conf.ConfigurationBuilder
 
 class TwitterStreamFactory extends StreamFactory[Status] {
-  /**
-    * Creates a DStream for a given connector config iff the connector config is supported by the stream factory.
-    * The param set allows the streaming context to be curried into the partial function which creates the stream.
-    *
-    * @param streamingContext The Spark Streaming Context
-    * @return A partial function for transforming a connector config
-    */
   override def createStream(streamingContext: StreamingContext): PartialFunction[ConnectorConfig, DStream[Status]] = {
     case ConnectorConfig("Twitter", params) =>
       val auth = new OAuthAuthorization(
