90cecefb7bb6f73a549663399d1b97bf685e8c9f
Clemens Wolff <clewolff@microsoft.com>
2017-06-29 12:19:38 -0700

    Add type definitions for all resolvers

--- src/resolvers-cassandra/Edges.js
+++ src/resolvers-cassandra/Edges.js
@@ -5,12 +5,24 @@ const Promise = require('promise');
 module.exports = {
   // ---------------------------------------------------------------------------------- mutations
 
+  /**
+   * @param {{input: {site: string, edges: Array<{name: string}>}}} args
+   * @returns {Promise.<{runTime: string, edges: Array<{name: string}>}>}
+   */
   removeKeywords(args, res){ // eslint-disable-line no-unused-vars
   },
 
+  /**
+   * @param {{input: {site: string, edges: Array<{name: string}>}}} args
+   * @returns {Promise.<{runTime: string, edges: Array<{name: string}>}>}
+   */
   addKeywords(args, res){ // eslint-disable-line no-unused-vars
   },
 
+  /**
+   * @param {{input: {site: string, targetBBox: number[], edges: Array<{name: string, type: string, alternatenames: string, coordinates: number[]}}>}}} args
+   * @returns {Promise.<{runTime: string, edges: Array<{name: string}>}>}
+   */
   saveLocations(args, res){ // eslint-disable-line no-unused-vars
     return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
       reject(
@@ -21,6 +33,10 @@ module.exports = {
     });
   },
 
+  /**
+   * @param {{input: {site: string, targetBBox: number[], edges: Array<{name: string, type: string, alternatenames: string, coordinates: number[]}}>}}} args
+   * @returns {Promise.<{runTime: string, edges: Array<{name: string}>}>}
+   */
   removeLocations(args, res){ // eslint-disable-line no-unused-vars
     return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
       reject(
@@ -33,18 +49,38 @@ module.exports = {
 
   // ------------------------------------------------------------------------------------ queries
 
+  /**
+   * @param {{site: string, query: string, fromDate: string, toDate: string, sourceFilter: string[]}} args
+   * @returns {Promise.<{runTime: string, edges: Array<{name: string}>}>}
+   */
   terms(args, res){ // eslint-disable-line no-unused-vars
   },
 
+  /**
+   * @param {{site: string, query: string}} args
+   * @returns {Promise.<{runTime: string, edges: Array<{name: string, coordinates: number[]}>}>}
+   */
   locations(args, res){ // eslint-disable-line no-unused-vars
   },
 
+  /**
+   * @param {{site: string, langCode: string, limit: number, timespan: string, zoomLevel: number, layertype: string, sourceFilter: string[], fromDate: string, toDate: string}} args
+   * @returns {Promise.<{runTime: string, edges: Array<{name: string, mentions: number, coordinates: number[], population: number}>}>}
+   */
   popularLocations(args, res){ // eslint-disable-line no-unused-vars
   },
 
+  /**
+   * @param {{site: string, fromDate: string, toDate: string, zoomLevel: number, limit: number, layertype: string, sourceFilter: string[], mainEdge: string}} args
+   * @returns {Promise.<{labels: Array<{name: string, mentions: number}>, graphData: Array<{date: string, edges: string[], mentions: number[]}>}>}
+   */
   timeSeries(args, res){ // eslint-disable-line no-unused-vars
   },
 
+  /**
+   * @param {{site: string, fromDate: string, toDate: string, limit: number, mainTerm: string, sourceFilter: string[]}} args
+   * @returns {Promise.<{sources: Array<{Name: string, Count: number, Source: string}>}>}
+   */
   topSources(args,res) { // eslint-disable-line no-unused-vars
   }
 };
--- src/resolvers-cassandra/Facts.js
+++ src/resolvers-cassandra/Facts.js
@@ -3,9 +3,17 @@
 module.exports = {
     // ------------------------------------------------------------------------------------ queries
 
+  /**
+   * @param {{pageSize: number, skip: number, tagFilter: string[]}} args
+   * @returns {Promise.<{runTime: string, type: string, facts: Array<{id: string, language: string, title: string, tags: string[], date: string, link: string, text: string, sources: string[]}>}>}
+   */
   list(args, res) { // eslint-disable-line no-unused-vars
   },
 
+  /**
+   * @param {{id: string}} args
+   * @returns {Promise.<{id: string, language: string, title: string, tags: string[], date: string, link: string, text: string, sources: string[]}>}
+   */
   get(args, res) { // eslint-disable-line no-unused-vars
   }
 };
