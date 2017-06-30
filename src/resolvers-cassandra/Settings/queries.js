56082e42f1a89d48b2c66788593209c0b2c3aa8c
JC Jimenez <jcjimenez@BigMac.guest.corp.microsoft.com>
2017-06-30 14:21:13 -0500

    Updating paths as per https://github.com/CatalystCode/project-fortis/issues/53.

--- src/resolvers-cassandra/Settings/queries.js
+++ src/resolvers-cassandra/Settings/queries.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Promise = require('promise');
-const facebookAnalyticsClient = require('../clients/facebook/FacebookAnalyticsClient');
+const facebookAnalyticsClient = require('../../clients/facebook/FacebookAnalyticsClient');
 
 /**
  * @param {{siteId: string}} args
