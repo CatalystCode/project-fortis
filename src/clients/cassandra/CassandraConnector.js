ed8e4fc5505466cdd681625bc0048cfd1a0e89cf
Smarker <Stephanie.Marker@Microsoft.com>
2017-06-30 12:29:33 -0400

    move exports to bottom and declares functions via function

--- src/clients/cassandra/CassandraConnector.js
+++ src/clients/cassandra/CassandraConnector.js
@@ -2,23 +2,23 @@
 
 const Promise = require('promise');
 
-module.exports = {
-  executeBatchMutations: executeBatchMutations,
-  executeQuery: executeQuery  
-};
-
 /** Execute a batch of mutations
  * @param {Array<{mutation: string, params: Array<string|number|map}>} mutations
  * @returns {Promise}
  */
-let executeBatchMutations = (mutations) => { // eslint-disable-line no-unused-vars
+function executeBatchMutations(mutations) { // eslint-disable-line no-unused-vars
   return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
     
   });
-};
+}
 
-let executeQuery = (query) => { // eslint-disable-line no-unused-vars
+function executeQuery(query) { // eslint-disable-line no-unused-vars
   return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
 
   });
+}
+
+module.exports = {
+  executeBatchMutations: executeBatchMutations,
+  executeQuery: executeQuery  
 };
