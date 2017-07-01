57328c13311712e70ec1663e2e99e5010a06be19
erikschlegel <marlene3>
2017-06-30 23:29:54 -0500

    Increasing deis delay due to cluster ingress router is taking longer than expected to resolve its dns.

--- ops/install-deis.sh
+++ ops/install-deis.sh
@@ -21,7 +21,7 @@ echo "Installing Deis on Cluster"
 helm install deis/workflow --name deis --namespace=deis --set global.storage=azure,azure.accountname="${DEIS_STORAGE_ACCOUNT_NAME}",azure.accountkey="${DEIS_STORAGE_ACCOUNT_KEY}",azure.registry_container=registry,azure.database_container=database,azure.builder_container=builder
 
 echo "Looking up DEIS_ROUTER_HOST_ROOT"
-sleep 300
+sleep 100
 
 while [[ -z ${DEIS_ROUTER_HOST_ROOT} ]]; do
    DEIS_ROUTER_HOST_ROOT=$(kubectl --namespace=deis get svc deis-router -o jsonpath='{.status.loadBalancer.ingress[*].ip}')
@@ -32,8 +32,8 @@ echo "DEIS_ROUTER_HOST_ROOT is set to ${DEIS_ROUTER_HOST_ROOT}"
 readonly DEIS_HOSTNAME_URL="http://deis.${DEIS_ROUTER_HOST_ROOT}.nip.io"
 readonly DEIS_BUILDER_HOSTNAME="deis-builder.${DEIS_ROUTER_HOST_ROOT}.nip.io"
 echo "Registering Deis Load Balancer"
-deis register "${DEIS_HOSTNAME_URL}" --username=deis-admin --password=test --email=newuser@deis.io
-deis login "${DEIS_HOSTNAME_URL}" --username=deis-admin --password=test
+sudo deis register "${DEIS_HOSTNAME_URL}" --username=deis-admin --password=test --email=newuser@deis.io
+#deis login "${DEIS_HOSTNAME_URL}" --username=deis-admin --password=test
 echo 'Registered deis users'
 deis perms:list
 echo 'Deis whoami'
