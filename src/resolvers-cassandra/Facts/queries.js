8b30f5b53b23909ac4117d140bd6059355435dd3
Clemens Wolff <clewolff@microsoft.com>
2017-07-03 12:41:04 -0700

    Implement FactSchema.list

--- src/resolvers-cassandra/Facts/queries.js
+++ src/resolvers-cassandra/Facts/queries.js
@@ -1,16 +1,57 @@
 'use strict';
 
+const Promise = require('promise');
+const cassandraConnector = require('../../clients/cassandra/CassandraConnector');
 const withRunTime = require('../shared').withRunTime;
 
 /**
  * @typedef {id: string, language: string, title: string, tags: string[], date: string, link: string, text: string, sources: string[]} Fact
  */
 
+function cassandraRowToFact(row) {
+  return {
+    id: row.id,
+    language: row.eventlangcode,
+    title: row.title,
+    tags: row.detectedkeywords,
+    date: row.event_time,
+    link: row.sourceurl,
+    text: row.messagebody,
+    sources: row.pipeline && [row.pipeline]
+  };
+}
+
+function appendDefaultFilters(query, params) {
+  query += ' AND pipeline IN ("TadaWeb", "Bing", "CustomEvent")';
+  return {query: query, params: params};
+}
+
+function makeListQuery(args) {
+  const tagsCondition = args.tagFilter.map(_ => 'detectedkeywords CONTAINS ?').join(' OR '); // eslint-disable-line no-unused-vars
+  const query = `SELECT * FROM fortis.events WHERE (${tagsCondition})`;
+  const params = args.tagFilter.slice();
+  return appendDefaultFilters(query, params);
+}
+
 /**
  * @param {{pageSize: number, skip: number, tagFilter: string[]}} args
  * @returns {Promise.<{runTime: string, type: string, facts: Fact[]}>}
  */
 function list(args, res) { // eslint-disable-line no-unused-vars
+  return new Promise((resolve, reject) => {
+    if (!args || !args.tagFilter || !args.tagFilter.length) return reject('No tags specified to fetch');
+
+    const query = makeListQuery(args);
+    return cassandraConnector.executeQuery(query.query, query.params)
+    .then(rows => {
+      const facts = rows.map(cassandraRowToFact);
+
+      resolve({
+        facts: facts
+      });
+    })
+    .catch(reject);
+  });
 }
 
 /**
