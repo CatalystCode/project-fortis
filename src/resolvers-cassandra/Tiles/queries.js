fc4d61e9150898d9e694fd74377e25c6062a277c
Clemens Wolff <clewolff@microsoft.com>
2017-07-04 12:28:38 -0700

    Implement TilesSchema.fetchTilesByBBox

--- src/resolvers-cassandra/Tiles/queries.js
+++ src/resolvers-cassandra/Tiles/queries.js
@@ -1,14 +1,108 @@
 'use strict';
 
 const Promise = require('promise');
+const cassandraConnector = require('../../clients/cassandra/CassandraConnector');
 const featureServiceClient = require('../../clients/locations/FeatureServiceClient');
 const withRunTime = require('../shared').withRunTime;
+
+/**
+ * @see http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Python
+ */
+function deg2num(lat_deg, lon_deg, zoom) {
+  const lat_rad = lat_deg * Math.PI / 180;
+  const n = Math.pow(2, zoom);
+  const xtile = Math.floor((lon_deg + 180) / 360 * n);
+  const ytile = Math.floor((1 - Math.log(Math.tan(lat_rad) + (1 / Math.cos(lat_rad))) / Math.PI) / 2 * n);
+  return {tilex: xtile, tiley: ytile, tilez: zoom};
+}
+
+function makeMap(iterable, keyFunc, valueFunc) {
+  const map = {};
+  iterable.forEach(item => {
+    const key = keyFunc(item);
+    const value = valueFunc(item);
+    map[key] = value;
+  });
+  return map;
+}
+
+function makeComputedTilesForTilesQuery(args, tiles) {
+  let params = [];
+  let clauses = [];
+
+  tiles.forEach(tile => {
+    clauses.push('(tilex = ? AND tiley = ? AND tilez = ?)');
+    params.push(tile.tilex);
+    params.push(tile.tiley);
+    params.push(tile.tilez);
+  });
+
+  const keywords = (args.filteredEdges || []).concat(args.mainEdge ? [args.mainEdge] : []);
+  if (keywords.length) {
+    clauses.push(`(${keywords.map(_ => '(keyword = ?)').join(' OR ')})`); // eslint-disable-line no-unused-vars
+    params = params.concat(keywords);
+  }
+
+  if (args.sourceFilter && args.sourceFilter.length) {
+    clauses.push(`(${args.sourceFilter.map(_ => '(pipeline = ?)').join(' OR ')})`); // eslint-disable-line no-unused-vars
+    params = params.concat(args.sourceFilter);
+  }
+
+  if (args.fromDate) {
+    clauses.push('(periodstartdate >= ?)');
+    params.push(args.fromDate);
+  }
+
+  if (args.toDate) {
+    clauses.push('(periodenddate <= ?)');
+    params.push(args.toDate);
+  }
+
+  if (args.timespan) {
+    clauses.push('(periodtype = ?)');
+    params.push(args.timespan);
+  }
+
+  const query = `SELECT tileid, computedfeatures FROM computedtiles WHERE ${clauses.join(' AND ')}`;
+  return {query: query, params: params};
+}
 
 /**
  * @param {{site: string, bbox: number[], mainEdge: string, filteredEdges: string[], timespan: string, zoomLevel: number, layertype: string, sourceFilter: string[], fromDate: string, toDate: string}} args
  * @returns {Promise.<{runTime: string, type: string, bbox: number[], features: Array<{type: string, coordinates: number[], properties: {mentionCount: number, location: string, population: number, neg_sentiment: number, pos_sentiment: number, tileId: string}}>}>}
  */
 function fetchTilesByBBox(args, res) { // eslint-disable-line no-unused-vars
+  return new Promise((resolve, reject) => {
+    if (!args || !args.bbox) return reject('No bounding box for which to fetch tiles specified');
+    if (!args || !args.zoomLevel) return reject('No zoom level for which to fetch tiles specified');
+    if (args.bbox.length !== 4) return reject('Invalid bounding box for which to fetch tiles specified');
+
+    const fence = {north: args.bbox[0], west: args.bbox[1], south: args.bbox[2], east: args.bbox[3]};
+    const bboxCornerPoints = [{latitude: fence.north, longitude: fence.west}, {latitude: fence.south, longitude: fence.west}, {latitude: fence.north, longitude: fence.east}, {latitude: fence.south, longitude: fence.east}];
+    const tilesInBbox = bboxCornerPoints.map(point => deg2num(point.latitude, point.longitude, args.zoomLevel));
+    const query = makeComputedTilesForTilesQuery(args, tilesInBbox);
+
+    cassandraConnector.executeQuery(query.query, query.params)
+    .then(rows => {
+      const rowsByTileId = makeMap(rows, row => row.tileid, row => row);
+      const features = Object.keys(rowsByTileId).map(tileId => {
+        const row = rowsByTileId[tileId];
+        return {
+          properties: {
+            pos_sentiment: row.computedfeatures && row.computedfeatures.sentiment && row.computedfeatures.sentiment.pos_avg,
+            neg_sentiment: row.computedfeatures && row.computedfeatures.sentiment && row.computedfeatures.sentiment.neg_avg,
+            mentionCount: row.computedfeatures && row.computedfeatures.mentions,
+            tileId: tileId
+          }
+        };
+      });
+
+      resolve({
+        features: features
+      });
+    })
+    .catch(reject);
+  });
 }
 
 /**
