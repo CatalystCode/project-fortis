0bc3e09aa7fa7798ed0e0b3d2bf3dd2dcd813dbc
Clemens Wolff <clewolff@microsoft.com>
2017-07-04 09:50:20 -0700

    Implement EdgesSchema.locations resolver

--- src/resolvers-cassandra/Edges/queries.js
+++ src/resolvers-cassandra/Edges/queries.js
@@ -1,6 +1,16 @@
 'use strict';
 
+const Promise = require('promise');
+const cassandraConnector = require('../../clients/cassandra/CassandraConnector');
+const featureServiceClient = require('../../clients/locations/featureServiceClient');
 const withRunTime = require('../shared').withRunTime;
+
+function makeSiteBboxQuery(args) {
+  return {
+    query: 'SELECT geofence FROM sitesettings WHERE sitename = ?',
+    params: [args.site]
+  };
+}
 
 /**
  * @param {{site: string, query: string, fromDate: string, toDate: string, sourceFilter: string[]}} args
@@ -14,6 +24,27 @@ function terms(args, res) { // eslint-disable-line no-unused-vars
  * @returns {Promise.<{runTime: string, edges: Array<{name: string, coordinates: number[]}>}>}
  */
 function locations(args, res) { // eslint-disable-line no-unused-vars
+  return new Promise((resolve, reject) => {
+    if (!args || !args.site) return reject('No site specified for which to lookup locations');
+
+    const siteQuery = makeSiteBboxQuery(args);
+    cassandraConnector.executeQuery(siteQuery.query, siteQuery.params)
+    .then(rows => {
+      if (!rows || !rows.length) return reject(`No geofence configured for site ${args.site}`);
+      if (rows.length > 1) return reject(`More than one geofence configured for site ${args.site}`);
+      if (!rows[0].geofence || rows[0].geofence.length !== 4) return reject(`Bad geofence for site ${args.site}`);
+
+      const bbox = rows[0].geofence;
+      featureServiceClient.fetchByBbox({north: bbox[0], west: bbox[1], south: bbox[2], east: bbox[3]})
+      .then(locations => {
+        resolve({
+          edges: locations.map(location => ({name: location.name, coordinates: location.bbox}))
+        });
+      })
+      .catch(reject);
+    })
+    .catch(reject);
+  });
 }
 
 /**
