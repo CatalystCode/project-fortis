427bee2457b9c9d0bdb8ab2f4535b14b34974748
erikschlegel <marlene3>
2017-07-05 15:41:52 -0500

    adding spark submit job and updated readme file.

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 [![Travis CI status](https://api.travis-ci.org/CatalystCode/fortisdeploy.svg?branch=master)](https://travis-ci.org/CatalystCode/fortisdeploy)
 
-# fortis-containers
-A repository of ACS containers used for Project Fortis and responsible to deploy the fortis containerized stack. 
+# deploy fortis pipeline
+Deploy your own Fortis pipeline to an azure subscription through a single click. 
 
 [![Deploy to Azure](http://azuredeploy.net/deploybutton.svg)](https://deploy.azure.com/?repository=https://github.com/catalystcode/fortis-containers/tree/master?ptmpl=azuredeploy.parameters.json)
+
+## Background
+Project Fortis is a data ingestion, analysis and visualization pipeline. The pipeline collects social media conversations and postings from the public web and darknet data sources. 
+
+![image](https://user-images.githubusercontent.com/7635865/27882830-e785819c-6193-11e7-9b27-5fc452f23b1a.png)
+
+## Deployment Prerequisite 
+* An existing azure subscription. You can create one for free [here](https://azure.microsoft.com/en-us/free/). 
+* Generate a Public / Private ssh key pair following [these](https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/) instructions. The contents from the generated `MyKey.pub` file will be used for the `SSh Public Key` field. 
+* An existing azure service principal. You can create one following these [instructions](https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-group-create-service-principal-portal). Your service principles `Application ID` will be used for the `Service Principal App ID` field, and the `Authentication Key` will be used for the `Service Principal App Key`. 
+
+## Fortis Monitored Data Sources
+* Public Web - Bing
+* Reddit
+* Twitter
+* Facebook
+* Instagram
+* Radio Broadcasts
+* ACLED
+
+## Post Deployment Instructions
+* Expect the deployment to run between 30 - 45 minutes, so grab a cup of coffee once you kickoff the deployment. 
+* Click on the `Manage your deployment` link once the deployment completes. Select the Deployment tab in the Azure Portal and copy the Fortis Admin Interface URL in the output section of the deployment summary. 
--- ops/setup-environment.sh
+++ ops/setup-environment.sh
@@ -12,14 +12,15 @@ readonly graphql_service_host="$6"
 readonly k8resource_group="$7"
 readonly fortis_interface_host="$8"
 readonly eh_conn_str="$9"
-readonly feature_service_db_conn_str="${10}"
+readonly feature_service_db_conn_str="$10"
+readonly fortis_central_directory="$11"
 
+readonly fortis_admin_interface="http://${fortis_interface_host}/#/site/${site_name}/admin"
 readonly default_language="en"
 readonly checkpoint_directory="HDFS://"
 readonly eh_path="published-messages"
 readonly eh_consumer_group="\$Default"
-readonly fortis_models_directory="https://fortiscentral.blob.core.windows.net/sentiment/"
-readonly fortis_central_directory="https://fortiscentral.blob.core.windows.net/"
+readonly fortis_models_directory="${fortis_central_directory}/sentiment/"
 
 kubectl create configmap "${spark_config_map_name}" --namespace spark --from-literal=FORTIS_CASSANDRA_HOST="${cassandra_host}" --from-literal=FORTIS_FEATURE_SERVICE_HOST="${feature_service_host}" --from-literal=DEFAULT_SITE_NAME="${site_name}" --from-literal=FORTIS_APPINSIGHTS_IKEY="${app_insights_id}" --from-literal=SPARK_DAEMON_MEMORY="${SPARK_DAEMON_MEMORY}" --from-literal=HA_PROGRESS_DIR="${checkpoint_directory}" --from-literal=DEFAULT_LANGUAGE="${default_language}" --from-literal=FORTIS_SERVICE_HOST="${graphql_service_host}" --from-literal=FORTIS_MODELS_DIRECTORY="${fortis_models_directory}" --from-literal=PUBLISH_EVENTS_EVENTHUB_CONNECTION_STRING="${eh_conn_str}" --from-literal=PUBLISH_EVENTS_EVENTHUB_PATH="${eh_path}" --from-literal=PUBLISH_EVENTS_EVENTHUB_PARTITION="${eh_consumer_group}"
 
@@ -37,11 +38,12 @@ deis config:set PUBLISH_EVENTS_EVENTHUB_CONNECTION_STRING="${eh_conn_str}"
 deis config:set PUBLISH_EVENTS_EVENTHUB_PATH="${eh_path}"
 deis config:set PUBLISH_EVENTS_EVENTHUB_PARTITION="${eh_consumer_group}"
 
-cd ../fortis-interfaces || exit 2
+cd ../fortis-interface || exit 2
 deis config:set APPINSIGHTS_INSTRUMENTATIONKEY="${app_insights_id}"
 deis config:set FORTIS_SERVICE_HOST="${graphql_service_host}"
 deis config:set DEFAULT_SITE_NAME="${site_name}"
 
 #Set the deployed service host url tag so we can output that on the deployment console to the user
-az resource tag --tags FORTIS_SERVICE_HOST="${graphql_service_host}" -g "${k8resource_group}" -n k8deisstorage --resource-type "Microsoft.Storage/storageAccounts"
-az resource tag --tags FORTIS_INTERFACE_HOST="${fortis_interface_host}" -g "${k8resource_group}" -n k8deisstorage --resource-type "Microsoft.Storage/storageAccounts"
+sudo az resource tag --tags FORTIS_SERVICE_HOST="${graphql_service_host}" -g "${k8resource_group}" -n k8deisstorage --resource-type "Microsoft.Storage/storageAccounts"
+sudo az resource tag --tags FORTIS_INTERFACE_HOST="${fortis_interface_host}" -g "${k8resource_group}" -n k8deisstorage --resource-type "Microsoft.Storage/storageAccounts"
+sudo az resource tag --tags FORTIS_ADMIN_INTERFACE_HOST="${fortis_admin_interface}" -g "${k8resource_group}" -n k8deisstorage --resource-type "Microsoft.Storage/storageAccounts"
