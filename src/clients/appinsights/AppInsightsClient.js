41ff5eb13386355cc73f01a5693fa2a280bd9667
Clemens Wolff <clewolff@microsoft.com>
2017-07-06 16:37:27 -0700

    Log application insights data also to console

--- src/clients/appinsights/AppInsightsClient.js
+++ src/clients/appinsights/AppInsightsClient.js
@@ -31,8 +31,6 @@ function trackTrace(level, localLogger) {
 }
 
 function trackDependency(promiseFunc, dependencyName, callName) {
-  if (!client) return promiseFunc;
-
   function dependencyTracker(...args) {
     return new Promise((resolve, reject) => {
       const start = new Date();
@@ -40,13 +38,19 @@ function trackDependency(promiseFunc, dependencyName, callName) {
       .then(returnValue => {
         const duration = new Date() - start;
         const success = true;
-        client.trackDependency(dependencyName, callName, duration, success);
+        if (client) {
+          client.trackDependency(dependencyName, callName, duration, success);
+        }
+        consoleLog(JSON.stringify({dependency: dependencyName, call: callName, duration: duration, success: success}));
         resolve(returnValue);
       })
       .catch(err => {
         const duration = new Date() - start;
         const success = false;
-        client.trackDependency(dependencyName, callName, duration, success);
+        if (client) {
+          client.trackDependency(dependencyName, callName, duration, success);
+        }
+        consoleLog(JSON.stringify({dependency: dependencyName, call: callName, duration: duration, success: success}));
         reject(err);
       });
     });
@@ -56,7 +60,6 @@ function trackDependency(promiseFunc, dependencyName, callName) {
 }
 
 function trackEvent(promiseFunc, eventName, extraPropsFunc) {
-  if (!client) return promiseFunc;
   extraPropsFunc = extraPropsFunc || ((returnValue, err) => ({})); // eslint-disable-line no-unused-vars
 
   function eventTracker(...args) {
@@ -67,14 +70,20 @@ function trackEvent(promiseFunc, eventName, extraPropsFunc) {
         const props = extraPropsFunc(returnValue, null);
         props.duration = new Date() - start;
         props.success = true;
-        client.trackEvent(eventName, props);
+        if (client) {
+          client.trackEvent(eventName, props);
+        }
+        consoleLog(JSON.stringify({event: eventName, properties: props}));
         resolve(returnValue);
       })
       .catch(err => {
         const props = extraPropsFunc(null, err);
         props.duration = new Date() - start;
         props.success = false;
-        client.trackEvent(eventName, props);
+        if (client) {
+          client.trackEvent(eventName, props);
+        }
+        consoleLog(JSON.stringify({event: eventName, properties: props}));
         reject(err);
       });
     });
