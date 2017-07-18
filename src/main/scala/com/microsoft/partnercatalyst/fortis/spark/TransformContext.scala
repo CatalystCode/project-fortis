2e022806cdb3308db2c67256f570b06640c00bfa
Kevin Hartman <kevin@hart.mn>
2017-07-18 13:54:45 -0400

    Add comment regarding non-broadcast vars in TransformContext.

--- src/main/scala/com/microsoft/partnercatalyst/fortis/spark/TransformContext.scala
+++ src/main/scala/com/microsoft/partnercatalyst/fortis/spark/TransformContext.scala
@@ -12,6 +12,9 @@ case class TransformContext(
   langToWatchlist: Broadcast[Map[String, List[String]]] = null,
   blacklist: Broadcast[List[BlacklistedTerm]] = null,
   locationsExtractorFactory: Broadcast[LocationsExtractorFactory] = null,
+
+  // The following objects have a small serialized forms. Consequently, we don't bother to broadcast them
+  // (instead, they're serialized into each task that uses them).
   imageAnalyzer: ImageAnalyzer = null,
   languageDetector: LanguageDetector = null,
   sentimentDetectorAuth: SentimentDetectorAuth = null
