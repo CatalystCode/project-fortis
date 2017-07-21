7859ca79b35ac7a73c3b94b5e4c1855280c6f019
Clemens Wolff <clewolff@microsoft.com>
2017-07-21 11:23:05 -0700

    Remove unused facts endpoint
    See https://github.com/CatalystCode/project-fortis-interfaces/search?q=%2Fapi%2Ffacts:
    the facts endpoint is never used by the client

--- src/resolvers-cassandra/Facts/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const queries = require('./queries');
-
-module.exports = {
-  list: queries.list,
-  get: queries.get
-};
--- src/schemas/FactSchema.js
+++
@@ -1,6 +0,0 @@
-const graphql = require('graphql');
-
-module.exports = graphql.buildSchema(`
-  type Query {
-  }
-`);
