5e8623692d96fc2fc959eaaa086128417c6fe001
JC Jimenez <jc.jimenez@microsoft.com>
2017-07-26 14:16:01 -0500

    Extended capabilities of Period class.

--- src/main/scala/com/microsoft/partnercatalyst/fortis/spark/analyzer/timeseries/ComputedTilesDAO.scala
+++ src/main/scala/com/microsoft/partnercatalyst/fortis/spark/analyzer/timeseries/ComputedTilesDAO.scala
@@ -8,22 +8,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
-trait ComputedTilesDAOConfig extends Serializable {
-  def retrospectivePeriodCountPerType(): Map[String, Int]
-}
-
-case class DefaultComputedTilesDAOConfig() extends ComputedTilesDAOConfig {
-  override def retrospectivePeriodCountPerType(): Map[String, Int] = Map(
-    (PeriodType.Minute.periodTypeName, 4*60), /* 4 hours */
-    (PeriodType.Hour.periodTypeName, 7*24), /* 7 days */
-    (PeriodType.Day.periodTypeName, 30), /* 4 weeks */
-    (PeriodType.Week.periodTypeName, 6*4), /* 6 months */
-    (PeriodType.Month.periodTypeName, 3*12), /* 3 years */
-    (PeriodType.Year.periodTypeName, 7) /* 7 years */
-  )
-}
-
-class ComputedTilesDAO(config: ComputedTilesDAOConfig = DefaultComputedTilesDAOConfig()) extends ComputedTilesSource {
+class ComputedTilesDAO(config: PeriodRetrospectiveConfig = DefaultPeriodRetrospectiveConfig()) extends ComputedTilesSource {
 
   override def latestTiles(sc: SparkContext,
                            topics: Set[(Option[String], Option[String], Option[String])],
@@ -33,7 +18,7 @@ class ComputedTilesDAO(config: ComputedTilesDAOConfig = DefaultComputedTilesDAOC
     val session = SparkSession.builder().appName(sc.appName).getOrCreate()
     val computedTilesTable = session.sparkContext.cassandraTable[ComputedTile]("fortis", "computedtiles")
     val topicTuples = topics.toSeq.map(TupleValue(_, null, null))
-    val periods = this.retrospectivePeriods(periodType, referenceTime)
+    val periods = periodType.retrospectivePeriods(referenceTime)
     computedTilesTable.select(
       "periodstartdate",
       "periodenddate",
@@ -54,14 +39,6 @@ class ComputedTilesDAO(config: ComputedTilesDAOConfig = DefaultComputedTilesDAOC
       "conjunctiontopics in ? and periodtype = ? and tilez in ? and period in ?",
       topicTuples, periodType.periodTypeName, tilez, periods
     )
-  }
-
-  private[timeseries] def retrospectivePeriods(periodType: PeriodType, referenceTime: Long): List[String] = {
-    val periodCountPerType = config.retrospectivePeriodCountPerType()
-    val periodWindowSize = periodCountPerType.getOrElse(periodType.periodTypeName, 7)
-    val latestPeriod = periodType.truncateTimestamp(referenceTime)
-    val startPeriod = latestPeriod - ((periodWindowSize - 1)* periodType.sizeInMilliseconds)
-    periodType.periodsBetweenTimestamps(startPeriod, latestPeriod).toList
   }
 
 }
--- src/main/scala/com/microsoft/partnercatalyst/fortis/spark/analyzer/timeseries/Period.scala
+++ src/main/scala/com/microsoft/partnercatalyst/fortis/spark/analyzer/timeseries/Period.scala
@@ -3,47 +3,82 @@ package com.microsoft.partnercatalyst.fortis.spark.analyzer.timeseries
 import java.text.SimpleDateFormat
 import java.util.{Calendar, Date, GregorianCalendar, TimeZone}
 
-case class Period(timestamp: Long, periodType: PeriodType) extends Serializable {
-  override def toString: String = periodType.formatTimestamp(this.timestamp)
+import org.joda.time._
+
+object Period {
+
+  def apply(timestamp: Long, periodType: PeriodType): Period = new Period(timestamp, periodType)
+
+  def apply(periodString: String): Period = {
+    val segments = periodString.split('-')
+    if (segments.length < 2) {
+      throw new RuntimeException("Invalid period string " + periodString)
+    }
+
+    val periodTypeName = segments(0)
+    val periodType = PeriodType.byName(periodTypeName)
+    val periodValue = periodString.substring(periodTypeName.length + 1)
+    val timestamp = periodType.parse(periodValue)
+    Period(timestamp, periodType)
+  }
+
+}
+
+class Period(timestamp: Long, periodType: PeriodType) extends Serializable {
+
+  private val defaultTimeZone = TimeZone.getTimeZone("UTC")
+
+  override def toString: String = periodType.format(this.timestamp)
+
+  def startTime(timeZone: TimeZone = defaultTimeZone): Long = periodType.truncate(this.timestamp, timeZone)
+
+  def endTime(timeZone: TimeZone = defaultTimeZone): Long = periodType.periodAfter(this.timestamp, timeZone)
+
+  def retrospectivePeriods(config: PeriodRetrospectiveConfig = new DefaultPeriodRetrospectiveConfig): List[String] = this.periodType.retrospectivePeriods(this.timestamp, config)
+
 }
 
 object PeriodType {
 
-  val Minute = PeriodType("minute", 60 * 1000, "yyyy-MM-dd HH:mm", Set())
+  val Minute = PeriodType("minute", Minutes.minutes(_), "yyyy-MM-dd HH:mm", Set())
 
-  val Hour = PeriodType("hour", 60L * Minute.sizeInMilliseconds, "yyyy-MM-dd HH", Set(Calendar.MINUTE))
+  val Hour = PeriodType("hour", Hours.hours(_), "yyyy-MM-dd HH", Set(Calendar.MINUTE))
 
-  val Day = PeriodType("day", 24L * Hour.sizeInMilliseconds, "yyyy-MM-dd", Set(Calendar.MINUTE, Calendar.HOUR_OF_DAY))
+  val Day = PeriodType("day", Days.days(_), "yyyy-MM-dd", Set(Calendar.MINUTE, Calendar.HOUR_OF_DAY))
 
-  val Week = PeriodType("week", 7L * Day.sizeInMilliseconds, "yyyy-w", Set(Calendar.MINUTE, Calendar.HOUR_OF_DAY, Calendar.WEEK_OF_YEAR))
+  val Week = PeriodType("week", Weeks.weeks(_), "yyyy-w", Set(Calendar.MINUTE, Calendar.HOUR_OF_DAY, Calendar.WEEK_OF_YEAR))
 
-  val Month = PeriodType("month", 30L * Day.sizeInMilliseconds, "yyyy-MM", Set(Calendar.MINUTE, Calendar.HOUR_OF_DAY, Calendar.DAY_OF_MONTH))
+  val Month = PeriodType("month", Months.months(_), "yyyy-MM", Set(Calendar.MINUTE, Calendar.HOUR_OF_DAY, Calendar.DAY_OF_MONTH))
 
-  val Year = PeriodType("year", 12L * Month.sizeInMilliseconds, "yyyy", Set(Calendar.MINUTE, Calendar.HOUR_OF_DAY, Calendar.DAY_OF_MONTH, Calendar.MONTH))
+  val Year = PeriodType("year", Years.years(_), "yyyy", Set(Calendar.MINUTE, Calendar.HOUR_OF_DAY, Calendar.DAY_OF_MONTH, Calendar.MONTH))
 
-  val all: Set[PeriodType] = Set(Minute, Hour, Day, Month, Year)
+  val all: Set[PeriodType] = Set(Minute, Hour, Day, Week, Month, Year)
+
+  val byName = all.groupBy(_.periodTypeName).mapValues(v=>v.head)
+
 }
 
-case class PeriodType(periodTypeName: String, sizeInMilliseconds: Long, format: String, truncateFields: Set[Int]) extends Serializable {
+case class PeriodType(periodTypeName: String, increment: Int=>ReadablePeriod, format: String, truncateFields: Set[Int]) extends Serializable {
 
-  def formatTimestamp(timestamp: Long, timeZone: Option[TimeZone] = None): String = {
-    format(new Date(timestamp), timeZone)
+  private val defaultTimeZone = TimeZone.getTimeZone("UTC")
+
+  def parse(periodValue: String, timeZone: TimeZone = defaultTimeZone): Long = {
+    val sdf = new SimpleDateFormat(this.format)
+    sdf.setTimeZone(timeZone)
+    sdf.parse(periodValue).getTime
   }
 
-  def format(date: Date, timeZone: Option[TimeZone] = None): String = {
+  def format(timestamp: Long, timeZone: TimeZone = defaultTimeZone): String = {
     val sdf = new SimpleDateFormat(this.format)
-    if (timeZone.isDefined) {
-      sdf.setTimeZone(timeZone.get)
-    }
-    val formattedDate = sdf.format(date)
+    sdf.setTimeZone(timeZone)
+
+    val formattedDate = sdf.format(new Date(timestamp))
     this.periodTypeName + "-" + formattedDate
   }
 
-  def truncateTimestamp(timestamp: Long, timeZone: Option[TimeZone] = None): Long = {
+  def truncate(timestamp: Long, timeZone: TimeZone = defaultTimeZone): Long = {
     val calendar = new GregorianCalendar()
-    if (timeZone.isDefined) {
-      calendar.setTimeZone(timeZone.get)
-    }
+    calendar.setTimeZone(timeZone)
     calendar.setTimeInMillis(timestamp)
     calendar.set(Calendar.MILLISECOND, 0)
     calendar.set(Calendar.SECOND, 0)
@@ -54,18 +89,55 @@ case class PeriodType(periodTypeName: String, sizeInMilliseconds: Long, format: 
     calendar.getTimeInMillis
   }
 
-  def truncate(date: Date, timeZone: Option[TimeZone] = None): Date = {
-    new Date(truncateTimestamp(date.getTime, timeZone))
+  def periodAfter(from: Long, timeZone: TimeZone = defaultTimeZone): Long = {
+    var currentDateTime = new DateTime(truncate(from))
+    currentDateTime.plus(increment(1)).getMillis
   }
 
-  def periodsBetweenTimestamps(from: Long, to: Long, timeZone: Option[TimeZone] = None): Iterator[String] = {
+  def periodsBetween(from: Long, to: Long, timeZone: TimeZone = defaultTimeZone): Iterator[String] = {
     val min = Math.min(from, to)
     val max = Math.max(from, to)
-    (min to max by this.sizeInMilliseconds).iterator.map(formatTimestamp(_, timeZone))
+
+    case class PeriodIterator(min: Long, max: Long) extends Iterator[String] {
+
+      var currentDateTime = new DateTime(min)
+
+      override def hasNext: Boolean = currentDateTime.isBefore(max+1)
+
+      override def next(): String = {
+        val period = format(currentDateTime.getMillis, timeZone)
+        currentDateTime = currentDateTime.plus(increment(1))
+        period
+      }
+
+    }
+    PeriodIterator(min, max)
   }
 
-  def periodsBetween(from: Date, to: Date, timeZone: Option[TimeZone] = None): Iterator[String] = {
-    periodsBetweenTimestamps(from.getTime, to.getTime, timeZone)
+  def retrospectivePeriods(referenceTime: Long, config: PeriodRetrospectiveConfig = new DefaultPeriodRetrospectiveConfig): List[String] = {
+    val periodCountPerType = config.retrospectivePeriodCountPerType()
+    val periodWindowSize = periodCountPerType.getOrElse(this.periodTypeName, 1)
+
+    var endPeriod = new DateTime(referenceTime)
+    var startPeriod = endPeriod.minus(this.increment(periodWindowSize-1))
+
+    this.periodsBetween(startPeriod.getMillis, endPeriod.getMillis).toList
   }
 
 }
+
+trait PeriodRetrospectiveConfig extends Serializable {
+  def retrospectivePeriodCountPerType(): Map[String, Int]
+}
+
+case class DefaultPeriodRetrospectiveConfig() extends PeriodRetrospectiveConfig {
+  override def retrospectivePeriodCountPerType(): Map[String, Int] = Map(
+    (PeriodType.Minute.periodTypeName, 4*60), /* 4 hours */
+    (PeriodType.Hour.periodTypeName, 7*24), /* 7 days */
+    (PeriodType.Day.periodTypeName, 30), /* 4 weeks */
+    (PeriodType.Week.periodTypeName, 6*4), /* 6 months */
+    (PeriodType.Month.periodTypeName, 3*12), /* 3 years */
+    (PeriodType.Year.periodTypeName, 7) /* 7 years */
+  )
+}
+
