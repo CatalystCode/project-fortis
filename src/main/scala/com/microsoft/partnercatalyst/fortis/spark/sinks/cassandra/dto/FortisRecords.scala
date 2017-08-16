76ed9a1003dbadfdead7cae0593911820ba08184
Clemens Wolff <clewolff@microsoft.com>
2017-08-16 14:42:43 -0700

    Fix whitespace

--- src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sinks/cassandra/dto/FortisRecords.scala
+++ src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sinks/cassandra/dto/FortisRecords.scala
@@ -19,126 +19,131 @@ case class Event(
 ) extends Serializable
 
 case class EventBatchEntry(
-                            eventid: String,
-                            pipelinekey: String) extends Serializable
+  eventid: String,
+  pipelinekey: String
+) extends Serializable
 
 case class EventTopics(
-                        pipelinekey: String,
-                        insertiontime: Long,
-                        eventid: String,
-                        externalsourceid: String,
-                        eventtime: Long,
-                        topic: String) extends Serializable
+  pipelinekey: String,
+  insertiontime: Long,
+  eventid: String,
+  externalsourceid: String,
+  eventtime: Long,
+  topic: String
+) extends Serializable
 
 case class EventPlaces(
-                        pipelinekey: String,
-                        insertiontime: Long,
-                        eventid: String,
-                        centroidlat: Double,
-                        centroidlon: Double,
-                        conjunctiontopic1: String,
-                        conjunctiontopic2: String,
-                        conjunctiontopic3: String,
-                        externalsourceid: String,
-                        eventtime: Long,
-                        placeid: String) extends Serializable
+  pipelinekey: String,
+  insertiontime: Long,
+  eventid: String,
+  centroidlat: Double,
+  centroidlon: Double,
+  conjunctiontopic1: String,
+  conjunctiontopic2: String,
+  conjunctiontopic3: String,
+  externalsourceid: String,
+  eventtime: Long,
+  placeid: String
+) extends Serializable
 
 case class PopularPlaceAggregate(
-                                  override val periodstartdate: Long,
-                                  override val externalsourceid: String,
-                                  override val periodenddate: Long,
-                                  override val periodtype: String,
-                                  override val period: String,
-                                  override val pipelinekey: String,
-                                  override val mentioncount: Long,
-                                  override val avgsentimentnumerator: Long,
-                                  override val avgsentiment: Double,
-                                  placeid: String,
-                                  centroidlat: Double,
-                                  centroidlon: Double,
-                                  conjunctiontopic1: String,
-                                  conjunctiontopic2: String,
-                                  conjunctiontopic3: String
-                        ) extends AggregationRecord with Serializable
+  override val periodstartdate: Long,
+  override val externalsourceid: String,
+  override val periodenddate: Long,
+  override val periodtype: String,
+  override val period: String,
+  override val pipelinekey: String,
+  override val mentioncount: Long,
+  override val avgsentimentnumerator: Long,
+  override val avgsentiment: Double,
+  placeid: String,
+  centroidlat: Double,
+  centroidlon: Double,
+  conjunctiontopic1: String,
+  conjunctiontopic2: String,
+  conjunctiontopic3: String
+) extends AggregationRecord with Serializable
 
 case class ComputedTile(
-                         override val periodstartdate: Long,
-                         override val externalsourceid: String,
-                         override val periodenddate: Long,
-                         override val periodtype: String,
-                         override val period: String,
-                         override val pipelinekey: String,
-                         override val mentioncount: Long,
-                         override val avgsentimentnumerator: Long,
-                         override val avgsentiment: Double,
-                         override val tilex: Int,
-                         override val tilez: Int,
-                         override val tiley: Int,
-                         detailtileid: String,
-                         conjunctiontopic1: String,
-                         conjunctiontopic2: String,
-                         conjunctiontopic3: String
-                       ) extends AggregationRecordTile with Serializable
+  override val periodstartdate: Long,
+  override val externalsourceid: String,
+  override val periodenddate: Long,
+  override val periodtype: String,
+  override val period: String,
+  override val pipelinekey: String,
+  override val mentioncount: Long,
+  override val avgsentimentnumerator: Long,
+  override val avgsentiment: Double,
+  override val tilex: Int,
+  override val tilez: Int,
+  override val tiley: Int,
+  detailtileid: String,
+  conjunctiontopic1: String,
+  conjunctiontopic2: String,
+  conjunctiontopic3: String
+) extends AggregationRecordTile with Serializable
 
 case class HeatmapEntry (
-                          mentioncountagg: Long,
-                          avgsentimentagg: Double
-                        )
+  mentioncountagg: Long,
+  avgsentimentagg: Double
+)
 
 case class PopularTopicAggregate(
-                                  override val periodstartdate: Long,
-                                  override val externalsourceid: String,
-                                  override val periodenddate: Long,
-                                  override val periodtype: String,
-                                  override val period: String,
-                                  override val pipelinekey: String,
-                                  override val mentioncount: Long,
-                                  override val avgsentimentnumerator: Long,
-                                  override val avgsentiment: Double,
-                                  override val tilex: Int,
-                                  override val tilez: Int,
-                                  override val tiley: Int,
-                                  topic: String
-                                ) extends AggregationRecordTile with Serializable
+  override val periodstartdate: Long,
+  override val externalsourceid: String,
+  override val periodenddate: Long,
+  override val periodtype: String,
+  override val period: String,
+  override val pipelinekey: String,
+  override val mentioncount: Long,
+  override val avgsentimentnumerator: Long,
+  override val avgsentiment: Double,
+  override val tilex: Int,
+  override val tilez: Int,
+  override val tiley: Int,
+  topic: String
+) extends AggregationRecordTile with Serializable
 
 case class ConjunctiveTopicAggregate(
-                                      conjunctivetopic: String,
-                                      externalsourceid: String,
-                                      mentioncount: Long,
-                                      period: String,
-                                      periodenddate: Long,
-                                      periodstartdate: Long,
-                                      periodtype: String,
-                                      pipelinekey: String,
-                                      tilex: Int,
-                                      tiley: Int,
-                                      tilez: Int,
-                                      topic: String
-                                    ) extends Serializable
+  conjunctivetopic: String,
+  externalsourceid: String,
+  mentioncount: Long,
+  period: String,
+  periodenddate: Long,
+  periodstartdate: Long,
+  periodtype: String,
+  pipelinekey: String,
+  tilex: Int,
+  tiley: Int,
+  tilez: Int,
+  topic: String
+) extends Serializable
 
 case class SiteSetting(
-                        sitename: String,
-                        geofence: Seq[Double],
-                        languages: Set[String],
-                        defaultzoom: Int,
-                        title: String,
-                        logo: String,
-                        translationsvctoken: String,
-                        cogspeechsvctoken: String,
-                        cogvisionsvctoken: String,
-                        cogtextsvctoken: String,
-                        insertiontime: Long
-                      )
+  sitename: String,
+  geofence: Seq[Double],
+  languages: Set[String],
+  defaultzoom: Int,
+  title: String,
+  logo: String,
+  translationsvctoken: String,
+  cogspeechsvctoken: String,
+  cogvisionsvctoken: String,
+  cogtextsvctoken: String,
+  insertiontime: Long
+)
 
 case class Stream(
-                   pipeline: String,
-                   streamid: Long,
-                   connector: String,
-                   params: Map[String, String])
+  pipeline: String,
+  streamid: Long,
+  connector: String,
+  params: Map[String, String]
+)
 
 case class TrustedSource(
-                          sourceid: String,
-                          sourcetype: String,
-                          connector: String,
-                          rank: Int,
-                          insertion_time: Long)
+  sourceid: String,
+  sourcetype: String,
+  connector: String,
+  rank: Int,
+  insertion_time: Long
+)
