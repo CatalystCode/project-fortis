e925f0dd3baabe3fc3a0ebb9eff920d78b57057f
Clemens Wolff <clewolff@microsoft.com>
2017-08-16 08:31:45 -0700

    Use multiple detectors to increase accuracy

--- src/main/scala/com/microsoft/partnercatalyst/fortis/spark/transforms/language/LocalLanguageDetector.scala
+++ src/main/scala/com/microsoft/partnercatalyst/fortis/spark/transforms/language/LocalLanguageDetector.scala
@@ -4,21 +4,28 @@ import com.microsoft.partnercatalyst.fortis.spark.logging.FortisTelemetry
 import com.optimaize.langdetect.LanguageDetectorBuilder
 import com.optimaize.langdetect.ngram.NgramExtractors
 import com.optimaize.langdetect.profiles.LanguageProfileReader
-import com.optimaize.langdetect.text.CommonTextObjectFactories
+import com.optimaize.langdetect.text.{CommonTextObjectFactories, TextObjectFactory}
 
 @SerialVersionUID(100L)
 class LocalLanguageDetector extends LanguageDetector {
   @transient private lazy val languageProfiles = new LanguageProfileReader().readAllBuiltIn
   @transient private lazy val languageDetector = LanguageDetectorBuilder.create(NgramExtractors.standard()).withProfiles(languageProfiles).build()
-  @transient private lazy val textObjectFactory = CommonTextObjectFactories.forDetectingOnLargeText()
+  @transient private lazy val largeTextFactory = CommonTextObjectFactories.forDetectingOnLargeText()
+  @transient private lazy val shortTextFactory = CommonTextObjectFactories.forDetectingShortCleanText()
 
   override def detectLanguage(text: String): Option[String] = {
     if (text.isEmpty) {
       return None
     }
 
-    val language = Option(languageDetector.detect(textObjectFactory.forText(text)).orNull).map(_.getLanguage)
+    var language = detectWithFactory(text, shortTextFactory)
+    if (language.isEmpty) language = detectWithFactory(text, largeTextFactory)
+
     FortisTelemetry.get().logLanguageDetection(language)
     language
+  }
+
+  private def detectWithFactory(text: String, factory: TextObjectFactory): Option[String] = {
+    Option(languageDetector.detect(factory.forText(text)).orNull).map(_.getLanguage)
   }
 }
