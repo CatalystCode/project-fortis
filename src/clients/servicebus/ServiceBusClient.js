57ca5764aff77fe57edc493da9dd00793f27d6c6
Smarker <Stephanie.Marker@Microsoft.com>
2017-09-01 15:13:40 -0400

    Change to send one message.

--- src/clients/servicebus/ServiceBusClient.js
+++ src/clients/servicebus/ServiceBusClient.js
@@ -2,49 +2,41 @@
 
 const Promise = require('promise');
 const azure = require('azure-sb'); 
-const asyncEachLimit = require('async/eachLimit');
 const trackDependency = require('../appinsights/AppInsightsClient').trackDependency;
 const SERVICE_BUS_CONNECTION_STRING = process.env.FORTIS_SB_CONN_STR;
-const MAX_CONCURRENT_BATCHES = process.env.MAX_CONCURRENT_BATCHES || 50;
 
 let client = azure.createServiceBusService(SERVICE_BUS_CONNECTION_STRING);
 
 /**
  * @param {string} queue
- * @param {Array<string>} messages
+ * @param {string} message
  * @returns {Promise}
  */
-function sendMessages(queue, messages) {
+function sendMessage(queue, message) {
   return new Promise((resolve, reject) => {
     if (!client) return reject('Failed to create service bus service. No service bus connection string provided.');
 
-    if (!messages || !messages.length) {
-      return reject('No messages to be sent to service bus.');
+    if (!message || !message.length) {
+      return reject('No message to be sent to service bus.');
     }
 
-    asyncEachLimit(messages, MAX_CONCURRENT_BATCHES, (message, asyncCallback) => {
-      try {
-        message => ({ body: JSON.stringify(message) });
-      } catch (exception) {
-        asyncCallback(exception);
-      }
+    try {
+      message => ({ body: JSON.stringify(message) });
+    } catch (exception) {
+      return reject(exception);
+    }
 
-      try {
-        client.sendQueueMessage(queue, message, (error) => {
-          if (error) asyncCallback(error);
-          else asyncCallback();
-        });
-      } catch (exception) {
-        asyncCallback(exception);
-      }
-    },
-    (error) => {
-      if (error) reject(error);
-      else resolve();
-    });
+    try {
+      client.sendQueueMessage(queue, message, (error) => {
+        if (error) reject(error);
+        else resolve(message);
+      });
+    } catch (exception) {
+      reject(exception);
+    }
   });
 }
 
 module.exports = {
-  sendMessages: trackDependency(sendMessages, 'ServiceBus', 'send')
+  sendMessages: trackDependency(sendMessage, 'ServiceBus', 'send')
 };
