e6b7cd50c1f1097ae51a36bd4977f8cc9f0900ec
Clemens Wolff <clewolff@microsoft.com>
2017-09-05 10:32:21 +0200

    Ensure that place centroids are set in Cassandra
    This requires two pieces:
    
    1) A feature recently was added to the featureService that enables the
    return of the centroid of a place using ST_Centroid in PostGIS. You can
    find a sample featureService request that includes place centroids in
    the response at https://aka.ms/b5ldsw.  This feature is now integrated
    in the Spark pipeline so that we augment all detected locations with
    their WOF ids and their centroids.
    
    2) The Cassandra aggregation logic and downstream GraphQL services
    assume that the centroid of places are always set. As such, we shouldn't
    represent the place coordinates in Spark as Optionals.

--- src/test/scala/com/microsoft/partnercatalyst/fortis/spark/transforms/locations/LocationsExtractorSpec.scala
+++ src/test/scala/com/microsoft/partnercatalyst/fortis/spark/transforms/locations/LocationsExtractorSpec.scala
@@ -1,6 +1,6 @@
 package com.microsoft.partnercatalyst.fortis.spark.transforms.locations
 
-import com.microsoft.partnercatalyst.fortis.spark.dto.Geofence
+import com.microsoft.partnercatalyst.fortis.spark.dto.{Geofence, Location}
 import com.microsoft.partnercatalyst.fortis.spark.transforms.locations.client.FeatureServiceClient
 import com.microsoft.partnercatalyst.fortis.spark.transforms.locations.dto.FeatureServiceFeature
 import org.scalatest.FlatSpec
@@ -8,18 +8,22 @@ import org.scalatest.FlatSpec
 object LocationsExtractorSpec {
   val idNyc = "wof-1234"
   val idManhattan = "wof-5678"
+  val latNyc = 123
+  val lngNyc = 456
+  val latManhattan = 78
+  val lngManhattan = 90
 
   val testLookup = Map(
-    "nyc" -> Set(idNyc),
-    "ny" -> Set(idNyc),
-    "new york" -> Set(idNyc),
-    "big apple" -> Set(idManhattan),
-    "manhattan" -> Set(idManhattan)
+    "nyc" -> Set(Location(idNyc, latitude = latNyc, longitude = lngNyc)),
+    "ny" -> Set(Location(idNyc, latitude = latNyc, longitude = lngNyc)),
+    "new york" -> Set(Location(idNyc, latitude = latNyc, longitude = lngNyc)),
+    "big apple" -> Set(Location(idManhattan, latitude = latManhattan, longitude = lngManhattan)),
+    "manhattan" -> Set(Location(idManhattan, latitude = latManhattan, longitude = lngManhattan))
   )
 }
 
 class TestLocationsExtractorFactory(client: FeatureServiceClient) extends LocationsExtractorFactory(client, geofence = null) {
-  def getLookup: Map[String, Set[String]] = lookup
+  def getLookup: Map[String, Set[Location]] = lookup
 }
 
 class TestFeatureServiceClient(givenBbox: Seq[FeatureServiceFeature], givenPoint: Seq[FeatureServiceFeature]) extends FeatureServiceClient("http://some/test/url") {
@@ -52,10 +56,9 @@ class LocationsExtractorSpec extends FlatSpec {
     val extractorFactory = new TestLocationsExtractorFactory(client).buildLookup()
     val lookup = extractorFactory.getLookup
 
-    assert(lookup == Map(
-      "new york" -> Set("id1", "id2"),
-      "gowanus heights" -> Set("id3")
-    ))
+    assert(lookup.size == 2)
+    assert(lookup("new york").map(_.wofId) == Set("id1", "id2"))
+    assert(lookup("gowanus heights").map(_.wofId) == Set("id3"))
   }
 
   it should "handle point queries" in {
