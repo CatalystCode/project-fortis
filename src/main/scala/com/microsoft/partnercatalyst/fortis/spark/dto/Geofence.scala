97cc2dddc63da65f4e1b5e8317270e3d9d300dd4
jcjimenez <jc.jimenez@microsoft.com>
2017-09-06 16:48:34 -0500

    Oops, reverting change to geofence.

--- src/main/scala/com/microsoft/partnercatalyst/fortis/spark/dto/Geofence.scala
+++ src/main/scala/com/microsoft/partnercatalyst/fortis/spark/dto/Geofence.scala
@@ -1,16 +1,3 @@
 package com.microsoft.partnercatalyst.fortis.spark.dto
 
-case class Geofence(north: Double, west: Double, south: Double, east: Double) {
-
-  def contains(location: Location): Boolean = {
-    location.latitude > south &&
-      location.latitude < north &&
-    location.longitude > west &&
-    location.longitude < east
-  }
-
-  def containsAtLeastOne(locations: List[Location]): Boolean = {
-    locations.find(contains).isDefined
-  }
-
-}
+case class Geofence(north: Double, west: Double, south: Double, east: Double)
