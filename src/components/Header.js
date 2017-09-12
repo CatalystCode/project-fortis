c0fe05ba0938d1a4d466c1294c8612b677d301d9
Clemens Wolff <clewolff@microsoft.com>
2017-09-12 09:28:57 +0200

    Refactor header
    - Make spacing consistent
    - Expose high-level structure by extracting methods

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,61 +5,94 @@ import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 
 export default class Header extends React.Component {
-  changeLanguage(event, index, value){
+  changeLanguage(event, index, value) {
     this.props.flux.actions.DASHBOARD.changeLanguage(value);
   }
 
   render() {
-    const { title, logo, language }  = this.props;
-    const nav = this.renderNav();
-
     return (
       <nav className="navbar navbar-trans" role="navigation">
-          <div>
-              <div className="navbar-header">
-                  <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapsible">
-                      <span className="sr-only">Toggle navigation</span>
-                      <span className="icon-bar"></span>
-                      <span className="icon-bar"></span>
-                      <span className="icon-bar"></span>
-                  </button>
-                  <a className="navbar-brand text-danger" href="#">
-                     {
-                         logo ? <img role="presentation" src={logo.startsWith("http:") ? logo : `${process.env.PUBLIC_URL}/images/${logo}`} style={{display: 'inline'}} height="48" /> 
-                            : undefined 
-                     }  
-                     <span className="brandLabel">{title}</span>
-                  </a>
-              </div>
-              <div className="navbar-collapse collapse" id="navbar-collapsible">
-                  {nav}
-                { this.props.settings && 
-                 <ul className="nav navbar-nav navbar-right">
-                     <SelectField underlineStyle={{ borderColor: '#337ab7', borderBottom: 'solid 3px' }}
-                                labelStyle={{ fontWeight: 600, color: '#2ebd59' }}
-                                value={language}
-                                autoWidth={true}
-                                style={{maxWidth:'60px'}}
-                                onChange={(event, index, value)=>this.changeLanguage(event, index, value)}>
-                                {this.props.supportedLanguages.map(lang => <MenuItem key={lang} value={lang} primaryText={lang} />)}
-                    </SelectField>
-                  </ul>
-                  }
-              </div>
+        <div>
+          { this.renderNavHeader() }
+          <div className="navbar-collapse collapse" id="navbar-collapsible">
+            { this.renderLeftNav() }
+            { this.renderRightNav() }
           </div>
-     </nav>
-      );
+        </div>
+      </nav>
+    );
   }
 
-  renderNav() {
-      let siteKey = this.props.siteKey;
-      return (
-          <ul className="nav navbar-nav navbar-left">
-              <li><Link to={`/site/${siteKey}/`} activeClassName="current">Dashboard</Link></li>
-              <li><Link to={`/site/${siteKey}/facts/`} activeClassName="current">Facts</Link></li>
-              {this.props.siteKey==="dengue" && <li><Link to={`/site/${siteKey}/predictions/`} activeClassName="current">Predictions</Link></li>}
-          </ul>
-      );
+  renderNavHeader() {
+    const { title, logo, language } = this.props;
+
+    return (
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapsible">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+        <a className="navbar-brand text-danger" href="#">
+          { logo && <img role="presentation" src={logo.startsWith("http:") ? logo : `${process.env.PUBLIC_URL}/images/${logo}`} style={{display: 'inline'}} height="48" /> }
+          <span className="brandLabel">{title}</span>
+        </a>
+      </div>
+    );
   }
 
+  renderLeftNav() {
+    return (
+      <ul className="nav navbar-nav navbar-left">
+        <li>{ this.renderDashboardLink() }</li>
+        <li>{ this.renderFactsLink() }</li>
+        { this.props.siteKey === "dengue" && <li>{ this.renderPredictionsLink() }</li> }
+      </ul>
+    );
+  }
+
+  renderDashboardLink() {
+    return (
+      <Link to={`/site/${this.props.siteKey}/`} activeClassName="current">Dashboard</Link>
+    );
+  }
+
+  renderFactsLink() {
+    return (
+      <Link to={`/site/${this.props.siteKey}/facts/`} activeClassName="current">Facts</Link>
+    );
+  }
+
+  renderPredictionsLink() {
+    return (
+      <Link to={`/site/${this.props.siteKey}/predictions/`} activeClassName="current">Predictions</Link>
+    );
+  }
+
+  renderRightNav() {
+    return (
+      <ul className="nav navbar-nav navbar-right">
+        { this.props.settings && <li>{ this.renderLanguagePicker() }</li> }
+      </ul>
+    );
+  }
+
+  renderLanguagePicker() {
+    const languages = this.props.supportedLanguages.map(lang =>
+      <MenuItem key={lang} value={lang} primaryText={lang} />
+    );
+
+    return (
+      <SelectField
+        underlineStyle={{ borderColor: '#337ab7', borderBottom: 'solid 3px' }}
+        labelStyle={{ fontWeight: 600, color: '#2ebd59' }}
+        value={this.props.language}
+        autoWidth={true}
+        style={{maxWidth:'60px'}}
+        onChange={(event, index, value) => this.changeLanguage(event, index, value)}>
+          {languages}
+      </SelectField>
+    );
+  }
 };
