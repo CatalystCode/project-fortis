71a119fcf66bfcb29089d0a9795b5d45049ff9c0
Clemens Wolff <clewolff@microsoft.com>
2017-09-12 09:42:13 +0200

    Fix relative path

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/Header.css';
+import '../../styles/Header.css';
 import { Link } from 'react-router';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
