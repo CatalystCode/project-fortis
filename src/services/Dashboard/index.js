3934d44f5d328ee9e98b95ba1180d0f4ee9ec491
Clemens Wolff <clewolff@microsoft.com>
2017-09-13 14:33:17 +0200

    Make logging less verbose

--- src/services/Dashboard/index.js
+++ src/services/Dashboard/index.js
@@ -67,7 +67,6 @@ export const SERVICES = {
 
     getHeatmapTiles(fromDate, toDate, zoomLevel, maintopic, tileid, periodType, 
                     dataSource, externalsourceid, conjunctivetopics, callback) {
-        console.log(`processing tile request [${maintopic}, ${fromDate}, ${toDate}, ${tileid}}]`)
         const topsourcespipelinekey = ActionMethods.DataSources(dataSource);
         const pipelinekeys = constants.DEFAULT_EXTERNAL_SOURCE === externalsourceid 
         ? [dataSource] : topsourcespipelinekey;
