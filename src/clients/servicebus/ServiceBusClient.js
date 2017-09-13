5d2b0a1edd9ffd37db5234f8bf2a46086deff5ab
Smarker <Stephanie.Marker@Microsoft.com>
2017-09-13 18:00:23 -0400

    Send service bus message on edit or create of site.

--- src/clients/servicebus/ServiceBusClient.js
+++ src/clients/servicebus/ServiceBusClient.js
@@ -5,8 +5,6 @@ const azure = require('azure-sb');
 const trackDependency = require('../appinsights/AppInsightsClient').trackDependency;
 const SERVICE_BUS_CONNECTION_STRING = process.env.FORTIS_SB_CONN_STR;
 
-let client = azure.createServiceBusService(SERVICE_BUS_CONNECTION_STRING);
-
 /**
  * @param {string} queue
  * @param {string} message
@@ -14,12 +12,18 @@ let client = azure.createServiceBusService(SERVICE_BUS_CONNECTION_STRING);
  */
 function sendStringMessage(queue, message) {
   return new Promise((resolve, reject) => {
+    let client;
+    try {
+      client = azure.createServiceBusService(SERVICE_BUS_CONNECTION_STRING);
+      if (!client) return reject('Failed to create service bus service. No service bus connection string provided.');
+    } catch (exception) {
+      return reject(exception);
+    }
+
     if (typeof message !== 'string') return reject('Message must be of type string.');
     if (!message || !message.length) {
       return reject('No message to be sent to service bus.');
     }
-
-    if (!client) return reject('Failed to create service bus service. No service bus connection string provided.');
     const serviceBusMessage = { body: message };
     try {
       client.sendQueueMessage(queue, serviceBusMessage, (error) => {
