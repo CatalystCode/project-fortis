CatalystCode/project-fortis

commit 872e1ae82213af482a7f42ee6a045c5807dd89b3

Watchlist final (#160)
* Add category field to topic.

* Add category field to topic queries and mutations.

* CChange translationsvctoken to translationSvcToken to match settings schema.

* Set err to error message body.

* Add appinsights logging of metrics.

* Add app insights logging to CassandraConnector.

* Add app insights logging to translator.

* Edit add and remove keywords services and add service bus calls.

* Service bus controller.

* Add app insights logging client.

* Update package.json

src/resolvers-cassandra/Settings/mutations.js
@@ -4,9 +4,10 @@ const Promise = require('promise');
 const uuid = require('uuid/v4');
 const cassandraConnector = require('../../clients/cassandra/CassandraConnector');
 const blobStorageClient = require('../../clients/storage/BlobStorageClient');
-const serviceBusClient = require('../../clients/servicebus/ServiceBusClient');
+const streamingController = require('../../clients/streaming/StreamingController');
 const { withRunTime, limitForInClause } = require('../shared');
-const { trackEvent } = require('../../clients/appinsights/AppInsightsClient');
+const { trackEvent, trackException } = require('../../clients/appinsights/AppInsightsClient');
+const loggingClient = require('../../clients/appinsights/LoggingClient');
 const apiUrlBase = process.env.FORTIS_CENTRAL_ASSETS_HOST || 'https://fortiscentral.blob.core.windows.net';
 const STREAM_PIPELINE_TWITTER = 'twitter';
 const STREAM_CONNECTOR_TWITTER = 'Twitter';
@@ -14,9 +15,6 @@ const STREAM_CONNECTOR_TWITTER = 'Twitter';
 const TRUSTED_SOURCES_CONNECTOR_TWITTER = 'Twitter';
 const TRUSTED_SOURCES_CONNECTOR_FACEBOOK = 'FacebookPage';
 const TRUSTED_SOURCES_RANK_DEFAULT = 10;
-
-const SERVICE_BUS_CONFIG_QUEUE = process.env.FORTIS_SB_CONFIG_QUEUE || 'configuration';
-const SERVICE_BUS_COMMAND_QUEUE = process.env.FORTIS_SB_COMMAND_QUEUE || 'command';
 
 function createOrReplaceSite(args, res) { // eslint-disable-line no-unused-vars
   return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
@@ -36,14 +34,17 @@ function _insertTopics(siteType) {
     blobStorageClient.fetchJson(uri)
     .then(response => {
       return response.map(topic => ({
-        query: `INSERT INTO fortis.watchlist (topicid,topic,lang_code,translations,insertiontime) 
+        query: `INSERT INTO fortis.watchlist (topicid,topic,lang_code,translations,insertiontime,category) 
                 VALUES (?, ?, ?, ?, toTimestamp(now()));`,
-        params: [uuid(), topic.topic, topic.lang_code, topic.translations]
+        params: [uuid(), topic.topic, topic.lang_code, topic.translations, topic.category]
       }));
     })
     .then(response => {
       mutations = response;
       return cassandraConnector.executeBatchMutations(response);
+    })
+    .then(() => {
+      streamingController.notifyWatchlistUpdate();
     })
     .then(() => {
       resolve({
@@ -97,7 +98,7 @@ function editSite(args, res) { // eslint-disable-line no-unused-vars
       }]);
     })
     .then(() => {
-      serviceBusClient.sendStringMessage(SERVICE_BUS_CONFIG_QUEUE, JSON.stringify({'dirty': 'settings'}));
+      streamingController.notifySiteSettingsUpdate();
     })
     .then(() => { 
       resolve({
@@ -155,7 +156,7 @@ function createSite(args, res) { // eslint-disable-line no-unused-vars
       }]);
     })
     .then(() => {
-      serviceBusClient.sendStringMessage(SERVICE_BUS_CONFIG_QUEUE, JSON.stringify({'dirty': 'settings'}));
+      streamingController.restartStreaming();
     })
     .then(() => { 
       resolve({
@@ -176,39 +177,68 @@ function createSite(args, res) { // eslint-disable-line no-unused-vars
 
 function removeKeywords(args, res) { // eslint-disable-line no-unused-vars
   return new Promise((resolve, reject) => {
-    if (!args || !args.edges || !args.edges.length) return reject('No keywords to remove specified');
+    if (!args || !args.input || !args.input.edges || !args.input.edges.length) {
+      loggingClient.logNoKeywordsToRemove();
+      return reject('No keywords to remove specified.');
+    } 
 
-    const mutations = args.edges.map(edge => ({
-      mutation: 'DELETE FROM fortis.watchlist WHERE keyword = ?',
-      params: [edge.name]
+    const mutations = args.input.edges.map(edge => ({
+      query: 'DELETE FROM fortis.watchlist WHERE topic = ? AND lang_code = ?',
+      params: [edge.name, edge.namelang]
     }));
 
     cassandraConnector.executeBatchMutations(mutations)
+    .then(() => {
+      streamingController.notifyWatchlistUpdate();
+    })
     .then(_ => { // eslint-disable-line no-unused-vars
       resolve({
-        edges: args.edges
+        edges: args.input.edges
       });
     })
-    .catch(reject);
+    .catch(error => {
+      trackException(error);
+      reject(error);
+    });
   });
 }
 
 function addKeywords(args, res) { // eslint-disable-line no-unused-vars
   return new Promise((resolve, reject) => {
-    if (!args || !args.edges || !args.edges.length) return reject('No keywords to add specified');
+    if (!args || !args.input || !args.input.edges || !args.input.edges.length) {
+      loggingClient.logNoKeywordsToAdd();
+      return reject('No keywords to add specified.');
+    }
 
-    const mutations = args.edges.map(edge => ({
-      mutation: 'INSERT INTO fortis.watchlist (keyword) VALUES (?)',
-      params: [edge.name]
-    }));
+    let mutations = [];
+    args.input.edges.forEach(edge => {
+      let params = paramEntryToMap(edge.translations);
+      mutations.push({
+        query: `INSERT INTO fortis.watchlist (
+          topic,
+          lang_code,
+          category,
+          insertiontime,
+          topicid,
+          translations
+        ) VALUES (?,?,?,dateof(now()),?,?)`,
+        params: [edge.name, edge.namelang, edge.category, edge.topicid, params]
+      });
+    });
 
     cassandraConnector.executeBatchMutations(mutations)
+    .then(() => {
+      streamingController.notifyWatchlistUpdate();
+    })
     .then(_ => { // eslint-disable-line no-unused-vars
       resolve({
-        edges: args.edges
+        edges: args.input.edges
       });
     })
-    .catch(reject);
+    .catch(error => {
+      trackException(error);
+      reject(error);
+    });
   });
 }
 
@@ -269,7 +299,7 @@ function modifyStreams(args, res) { // eslint-disable-line no-unused-vars
 
     cassandraConnector.executeBatchMutations(mutations)
     .then(() => {
-      serviceBusClient.sendStringMessage(SERVICE_BUS_COMMAND_QUEUE, JSON.stringify({'dirty': 'streams'}));
+      streamingController.restartStreaming();
     })
     .then(() => {
       resolve({
@@ -295,7 +325,7 @@ function removeStreams(args, res) { // eslint-disable-line no-unused-vars
 
     cassandraConnector.executeBatchMutations(deletions)
     .then(() => {
-      serviceBusClient.sendStringMessage(SERVICE_BUS_COMMAND_QUEUE, JSON.stringify({'dirty': 'streams'}));
+      streamingController.restartStreaming();
     })
     .then(() => {
       resolve({
@@ -496,7 +526,7 @@ function modifyBlacklist(args, res) { // eslint-disable-line no-unused-vars
 
     cassandraConnector.executeBatchMutations(mutations)
     .then(() => {
-      return serviceBusClient.sendStringMessage(SERVICE_BUS_CONFIG_QUEUE, JSON.stringify({'dirty': 'blacklist'}));
+      streamingController.notifyBlacklistUpdate();
     })
     .then(() => resolve({ filters: filterRecords }))
     .catch(reject);
@@ -522,7 +552,7 @@ function removeBlacklist(args, res) { // eslint-disable-line no-unused-vars
 
     cassandraConnector.executeQuery(query, params)
     .then(() => {
-      return serviceBusClient.sendStringMessage(SERVICE_BUS_CONFIG_QUEUE, JSON.stringify({'dirty': 'blacklist'}));
+      streamingController.notifyBlacklistUpdate();
     })
     .then(() => {
       resolve({
@@ -539,8 +569,8 @@ module.exports = {
   removeSite: trackEvent(removeSite, 'removeSite'),
   modifyStreams: trackEvent(withRunTime(modifyStreams), 'modifyStreams'),
   removeStreams: trackEvent(removeStreams, 'removeStreams'),
-  removeKeywords: trackEvent(withRunTime(removeKeywords), 'removeKeywords'),
-  addKeywords: trackEvent(withRunTime(addKeywords), 'addKeywords'),
+  removeKeywords: trackEvent(withRunTime(removeKeywords), 'removeKeywords', loggingClient.removeKeywordsExtraProps(), loggingClient.keywordsExtraMetrics()),
+  addKeywords: trackEvent(withRunTime(addKeywords), 'addKeywords', loggingClient.addKeywordsExtraProps(), loggingClient.keywordsExtraMetrics()),
   editSite: trackEvent(withRunTime(editSite), 'editSite'),
   modifyFacebookPages: trackEvent(withRunTime(modifyFacebookPages), 'modifyFacebookPages'),
   removeFacebookPages: trackEvent(withRunTime(removeFacebookPages), 'removeFacebookPages'),

src/clients/appinsights/AppInsightsClient.js
@@ -21,12 +21,19 @@ function setup() {
   }
 }
 
-function trackTrace(level, localLogger) {
-  return (message) => {
+function trackException(exception) {
+  if (client) {
+    client.trackException(exception);
+  }
+}
+
+function trackTrace(severityLevel, localLogger) {
+  return (message, properties) => {
     if (client) {
-      client.trackTrace(message, level);
+      if (!properties) properties = '';
+      client.trackTrace(message, severityLevel, properties);
     }
-    localLogger(message);
+    localLogger(message, properties);
   };
 }
 
@@ -60,31 +67,34 @@ function trackDependency(promiseFunc, dependencyName, callName) {
   return dependencyTracker;
 }
 
-function trackEvent(promiseFunc, eventName, extraPropsFunc) {
+function trackEvent(promiseFunc, eventName, extraPropsFunc, extraMetricsFunc) {
   extraPropsFunc = extraPropsFunc || ((returnValue, err) => ({})); // eslint-disable-line no-unused-vars
+  extraMetricsFunc = extraMetricsFunc || ((returnValue, err) => ({})); // eslint-disable-line no-unused-vars
 
   function eventTracker(...args) {
     return new Promise((resolve, reject) => {
       const start = new Date();
       promiseFunc(...args)
       .then(returnValue => {
-        const props = extraPropsFunc(returnValue, null);
-        props.duration = new Date() - start;
-        props.success = true;
+        const properties = extraPropsFunc(returnValue, null);
+        properties.duration = new Date() - start;
+        properties.success = true;
+        const metrics = extraMetricsFunc(returnValue, null);
         if (client) {
-          client.trackEvent(eventName, props);
+          client.trackEvent(eventName, properties, metrics);
         }
-        console.log(JSON.stringify({event: eventName, properties: props, args: args && args.length && args[0]}));
+        console.log(JSON.stringify({event: eventName, properties, metrics, args: args && args.length && args[0]}));
         resolve(returnValue);
       })
       .catch(err => {
-        const props = extraPropsFunc(null, err);
-        props.duration = new Date() - start;
-        props.success = false;
+        const properties = extraPropsFunc(null, err);
+        properties.duration = new Date() - start;
+        properties.success = false;
+        const metrics = extraMetricsFunc(null, err);
         if (client) {
-          client.trackEvent(eventName, props);
+          client.trackEvent(eventName, properties);
         }
-        console.error(JSON.stringify({event: eventName, properties: props, err, args: args && args.length && args[0]}));
+        console.error(JSON.stringify({event: eventName, properties, metrics, err, args: args && args.length && args[0]}));
         reject(err);
       })
       .catch(reject);
@@ -94,8 +104,17 @@ function trackEvent(promiseFunc, eventName, extraPropsFunc) {
   return eventTracker;
 }
 
+function trackSyncEvent(eventName, properties, metrics) {
+  if (client) {
+    client.trackEvent(eventName, properties, metrics);
+  }
+}
+
 module.exports = {
-  trackDependency: trackDependency,
-  trackEvent: trackEvent,
-  setup: setup
+  setup,
+  trackException,
+  trackTrace,
+  trackDependency,
+  trackEvent,
+  trackSyncEvent
 };

src/clients/appinsights/AppInsightsConstants.js
@@ -1,0 +1,16 @@
+const CLIENTS = {
+  cassandra: 'cassandra',
+  appInsights: 'appInsights',
+  eventHub: 'eventHub',
+  facebookAnalytics: 'facebookAnalytics',
+  featureService: 'featureService',
+  tileService: 'tileService',
+  postres: 'postgres',
+  serviceBus: 'serviceBus',
+  blobStorage: 'blobStorage',
+  translator: 'translator'
+};
+
+module.exports = {
+  CLIENTS: CLIENTS
+};

src/clients/appinsights/LoggingClient.js
@@ -1,0 +1,125 @@
+const { trackSyncEvent } = require('../appinsights/AppInsightsClient');
+const constants = require('../appinsights/AppInsightsConstants');
+
+function logCassandraClientUndefined() {
+  trackSyncEvent('cassandra', {
+    client: constants.CLIENTS.cassandra,
+    operation: 'connect',
+    success: 'false'
+  }, {});
+}
+
+function logNoMutationsDefined() {
+  trackSyncEvent('cassandra', { 
+    client: constants.CLIENTS.cassandra,
+    operation: 'mutate',
+    success: 'false'
+  }, {
+    numToMutate: 0
+  });
+}
+
+function logExecuteQueryError() {
+  trackSyncEvent('cassandra', { 
+    client: constants.CLIENTS.cassandra,
+    operation: 'query',
+    success: 'false'
+  }, {
+    totalRows: 0
+  });
+}
+
+function termsExtraProps() {
+  return () => ({
+    operation: 'query',
+    table: 'watchlist',
+    success: 'true'
+  });
+}
+
+function addKeywordsExtraProps() {
+  return () => ({
+    operation: 'modify',
+    table: 'watchlist',
+    success: 'true'
+  });
+}
+
+function removeKeywordsExtraProps() {
+  return () => ({
+    operation: 'remove',
+    table: 'watchlist',
+    success: 'true'
+  });
+}
+
+function keywordsExtraMetrics() {
+  return (graphqlResult) => {
+    const totalRows = graphqlResult.edges.length; //TODO
+    return {
+      totalRows
+    };
+  };
+}
+
+function logNoKeywordsToAdd() {
+  trackSyncEvent('cassandra', { 
+    client: constants.CLIENTS.cassandra,
+    operation: 'modify',
+    table: 'watchlist',
+    success: false
+  }, {
+    numToMutate: 0
+  });
+}
+
+function logNoKeywordsToRemove() {
+  trackSyncEvent('cassandra', { 
+    client: constants.CLIENTS.cassandra,
+    operation: 'remove',
+    table: 'watchlist',
+    success: 'false'
+  },{
+    numToMutate: 0
+  });
+}
+
+function translateExtraProps() {
+  return () => ({
+    operation: 'translate',
+    success: 'true'
+  });
+}
+
+function translateExtraMetrics() {
+  return (graphqlResult) => {
+    const totalRows = graphqlResult && graphqlResult.translate && graphqlResult.translate.words && graphqlResult.translate.words.length;
+    return {
+      totalRows
+    };
+  };
+}
+
+function translateWordsExtraMetrics() {
+  return (graphqlResult) => {
+    const totalRows = graphqlResult && graphqlResult.translateWords && graphqlResult.translateWords.words && graphqlResult.translateWords.words.length;
+    return {
+      totalRows
+    };
+  };
+}
+
+module.exports = {
+  logCassandraClientUndefined,
+  logNoMutationsDefined,
+  logExecuteQueryError,
+  termsExtraProps,
+  addKeywordsExtraProps,
+  removeKeywordsExtraProps,
+  keywordsExtraMetrics,
+  logNoKeywordsToAdd,
+  logNoKeywordsToRemove,
+  translateExtraProps,
+  translateExtraMetrics,
+  translateWordsExtraMetrics
+};

src/clients/cassandra/CassandraConnector.js
@@ -4,7 +4,8 @@ const Promise = require('promise');
 const cassandra = require('cassandra-driver');
 const asyncEachLimit = require('async/eachLimit');
 const chunk = require('lodash/chunk');
-const trackDependency = require('../appinsights/AppInsightsClient').trackDependency;
+const { trackDependency, trackException } = require('../appinsights/AppInsightsClient');
+const loggingClient = require('../appinsights/LoggingClient');
 
 const FETCH_SIZE = process.env.FETCH_SIZE || 1000;
 const MAX_OPERATIONS_PER_BATCH = process.env.MAX_OPERATIONS_PER_BATCH || 10;
@@ -38,8 +39,15 @@ const client = new cassandra.Client(options);
  */
 function executeBatchMutations(mutations) {
   return new Promise((resolve, reject) => {
-    if (!client) return reject('No Cassandra client defined');
-    if (!mutations || !mutations.length) return reject('No mutations defined');
+    if (!client) {
+      loggingClient.logCassandraClientUndefined();
+      return reject('No Cassandra client defined');
+    }
+
+    if (!mutations || !mutations.length) {
+      loggingClient.logNoMutationsDefined();
+      return reject('No mutations defined');
+    }
   
     let chunkedMutations = chunk(mutations, MAX_OPERATIONS_PER_BATCH);
 
@@ -57,7 +65,10 @@ function executeBatchMutations(mutations) {
       }
     },
     (err) => {
-      if (err) reject(err);
+      if (err) {
+        trackException(err);
+        reject(err);
+      }
       else resolve({numBatchMutations: chunkedMutations.length});
     });
   });
@@ -71,6 +82,11 @@ function executeBatchMutations(mutations) {
  */
 function executeQuery(query, params, options) {
   return new Promise((resolve, reject) => {
+    if (!client) {
+      loggingClient.logCassandraClientUndefined();
+      return reject('No Cassandra client defined');
+    }
+
     try {
       const rows = [];
       const stream = client.stream(query, params, options)
@@ -81,12 +97,16 @@ function executeQuery(query, params, options) {
           }
         })
         .on('error', err => {
-          if (err) reject(err);
+          if (err) {
+            loggingClient.logExecuteQueryError();
+            reject(err);
+          }
         })
         .on('end', () => {
           resolve(rows);
         });
     } catch (exception) {
+      trackException(exception);
       reject(exception);
     }
   });
@@ -101,6 +121,11 @@ function executeQuery(query, params, options) {
  */
 function executeQueryWithPageState(query, params, pageState, fetchSize) {
   return new Promise((resolve, reject) => {
+    if (!client) {
+      loggingClient.logCassandraClientUndefined();
+      return reject('No Cassandra client defined');
+    }
+
     const DEFAULT_FETCH = 15;
 
     let options = {
@@ -116,7 +141,10 @@ function executeQueryWithPageState(query, params, pageState, fetchSize) {
       pageState: result.pageState, 
       rows: result.rows
     }))
-    .catch(reject);
+    .catch(error => {
+      trackException(error);
+      reject(error);
+    });
   });
 }
 

src/clients/servicebus/ServiceBusClient.js
@@ -1,41 +1,0 @@
-'use strict';
-
-const Promise = require('promise');
-const azure = require('azure-sb'); 
-const trackDependency = require('../appinsights/AppInsightsClient').trackDependency;
-const SERVICE_BUS_CONNECTION_STRING = process.env.FORTIS_SB_CONN_STR;
-
-/**
- * @param {string} queue
- * @param {string} message
- * @returns {Promise}
- */
-function sendStringMessage(queue, message) {
-  return new Promise((resolve, reject) => {
-    if (typeof message !== 'string') return reject('Message must be of type string.');
-    if (!message || !message.length) {
-      return reject('No message to be sent to service bus.');
-    }
-
-    let client;
-    try {
-      client = azure.createServiceBusService(SERVICE_BUS_CONNECTION_STRING);
-    } catch (exception) {
-      return reject(exception);
-    }
-
-    const serviceBusMessage = { body: message };
-    try {
-      client.sendQueueMessage(queue, serviceBusMessage, (error) => {
-        if (error) reject(error);
-        else resolve(serviceBusMessage);
-      });
-    } catch (exception) {
-      reject(exception);
-    }
-  });
-}
-
-module.exports = {
-  sendStringMessage: trackDependency(sendStringMessage, 'ServiceBus', 'send')
-};

src/clients/streaming/StreamingController.js
@@ -1,0 +1,63 @@
+'use strict';
+
+const Promise = require('promise');
+const azure = require('azure-sb'); 
+const trackDependency = require('../appinsights/AppInsightsClient').trackDependency;
+const SERVICE_BUS_CONNECTION_STRING = process.env.FORTIS_SB_CONN_STR;
+
+const SERVICE_BUS_CONFIG_QUEUE = process.env.FORTIS_SB_CONFIG_QUEUE || 'configuration';
+const SERVICE_BUS_COMMAND_QUEUE = process.env.FORTIS_SB_COMMAND_QUEUE || 'command'; 
+
+function restartStreaming() {
+  return notifyUpdate(SERVICE_BUS_COMMAND_QUEUE, { 'dirty': 'streams' });
+}
+
+function notifyWatchlistUpdate() {
+  return notifyUpdate(SERVICE_BUS_CONFIG_QUEUE, { 'dirty': 'watchlist' });
+}
+
+function notifyBlacklistUpdate() {
+  return notifyUpdate(SERVICE_BUS_CONFIG_QUEUE, { 'dirty': 'blacklist' });
+}
+
+function notifySiteSettingsUpdate() {
+  return notifyUpdate(SERVICE_BUS_CONFIG_QUEUE, { 'dirty': 'sitesettings' });
+}
+
+function notifyUpdate(queue, properties) {
+  return new Promise((resolve, reject) => {
+    const serviceBusMessage = { 
+      customProperties: properties
+    };
+
+    sendQueueMessage(queue, serviceBusMessage)
+      .then(resolve)
+      .catch(reject);
+  });
+}
+
+function sendQueueMessage(queue, serviceBusMessage) {
+  return new Promise((resolve, reject) => {
+    let client;
+    try {
+      client = azure.createServiceBusService(SERVICE_BUS_CONNECTION_STRING);
+    } catch (exception) {
+      return reject(exception);
+    }
+    try {
+      client.sendQueueMessage(queue, serviceBusMessage, (error) => {
+        if (error) reject(error);
+        else resolve(serviceBusMessage);
+      });
+    } catch (exception) {
+      reject(exception);
+    }
+  });
+}
+
+module.exports = {
+  restartStreaming: trackDependency(restartStreaming, 'ServiceBus', 'send'),
+  notifyWatchlistUpdate: trackDependency(notifyWatchlistUpdate, 'ServiceBus', 'send'),
+  notifyBlacklistUpdate: trackDependency(notifyBlacklistUpdate, 'ServiceBus', 'send'),
+  notifySiteSettingsUpdate: trackDependency(notifySiteSettingsUpdate, 'ServiceBus', 'send')
+};

src/clients/translator/MsftTranslator.js
@@ -3,8 +3,9 @@
 const xml2js = require('xml2js');
 const Promise = require('promise');
 const request = require('request');
-const trackDependency = require('../appinsights/AppInsightsClient').trackDependency;
+const { trackEvent, trackException } = require('../appinsights/AppInsightsClient');
 const deprecated = require('./deprecated');
+const loggingClient = require('../appinsights/LoggingClient');
 
 const TOKEN_URL_BASE = process.env.TRANSLATION_SERVICE_TOKEN_HOST || 'https://api.cognitive.microsoft.com';
 const TRANSLATOR_URL_BASE = process.env.TRANSLATION_SERVICE_TRANSLATOR_HOST || 'https://api.microsofttranslator.com';
@@ -31,6 +32,13 @@ function getAccessTokenForTranslation(token) {
 
     request.post(POST, (err, response, body) => {
       if (response.statusCode !== 200 || err) {
+        try {
+          err = JSON.parse(body);
+        } catch (exception) {
+          trackException(err);
+          return reject(err);
+        }
+        trackException(err);
         return reject(err);
       } else {
         resolve({
@@ -59,6 +67,7 @@ function TranslateSentenceArrayWithToken(access_token, wordsToTranslate, fromLan
   return new Promise((resolve, reject) => {
     request.post(POST, (err, response, body) => {
       if (err || !response || response.statusCode !== 200) {
+        trackException(err);
         return reject(err || 'Failed to pull data from: ' + JSON.stringify(response));
       } else {
         xml2js.parseString(body, (err, result) => {
@@ -66,6 +75,7 @@ function TranslateSentenceArrayWithToken(access_token, wordsToTranslate, fromLan
             const translatedPhrases = result.ArrayOfTranslateArrayResponse.TranslateArrayResponse;
             resolve(wordsToTranslate.map((phrase, index) => Object.assign({}, { originalSentence: phrase, translatedSentence: translatedPhrases[index].TranslatedText })));
           } else {
+            trackException(err);
             return reject(err || 'Failed to pull data from: ' + JSON.stringify(response));
           }
         });
@@ -86,12 +96,14 @@ function TranslateWithToken(access_token, sentence, fromLanguage, toLanguage) {
   return new Promise((resolve, reject) => {
     request(options, function (err, response, body) {
       if (err || !response || response.statusCode !== 200) {
+        trackException(err);
         return reject(err || 'Failed to pull data from: ' + JSON.stringify(response));
       } else {
         xml2js.parseString(body, (err, result) => {
           if (!err & !!result && !!result['string'] && !!result['string']['_']) {
             resolve(result['string']['_']);
           } else {
+            trackException(err);
             return reject(err || 'Failed to pull data from: ' + JSON.stringify(response));
           }
         });
@@ -100,15 +112,21 @@ function TranslateWithToken(access_token, sentence, fromLanguage, toLanguage) {
   });
 }
 
-function translateSentenceArray(wordsToTranslate, fromLanguage, toLanguage) {
+function translateSentenceArray(token, wordsToTranslate, fromLanguage, toLanguage) {
   return new Promise((resolve, reject) => {
-    getAccessTokenForTranslation()
+    getAccessTokenForTranslation(token)
       .then(authBody => {
         TranslateSentenceArrayWithToken(authBody.token, wordsToTranslate, fromLanguage, toLanguage)
           .then(result => resolve({ translatedSentence: result }))
-          .catch(reject);
+          .catch(error => {
+            trackException(error);
+            reject(error);
+          });
       })
-      .catch(reject);
+      .catch(error => {
+        trackException(error);
+        reject(error);
+      });
   });
 }
 
@@ -118,13 +136,19 @@ function translate(token, sentence, fromLanguage, toLanguage) {
       .then(authBody => {
         TranslateWithToken(authBody.token, sentence, fromLanguage, toLanguage)
           .then(result => resolve({ translatedSentence: result }))
-          .catch(reject);
+          .catch(error => {
+            trackException(error);
+            reject(error);
+          });
       })
-      .catch(reject);
+      .catch(error => {
+        trackException(error);
+        reject(error);
+      });
   });
 }
 
 module.exports = {
-  translateSentenceArray: trackDependency(translateSentenceArray, 'MsftTranslator', 'translateSentenceArray'),
-  translate: trackDependency(translate, 'MsftTranslator', 'translate')
+  translateSentenceArray: trackEvent(translateSentenceArray, 'MicrosoftTranslator', loggingClient.translateExtraProps(), loggingClient.translateWordsExtraMetrics()),
+  translate: trackEvent(translate, 'MicrosoftTranslator', loggingClient.translateExtraProps(), loggingClient.translateExtraMetrics())
 };

commit 0a39146eb71e53670f056fe7a92f5d81c6f3da96

Fix create_site.py failing
The create-site call was failing with an error of "Unmatched column
names/values". This is due to a regression introduced in 872e1ae8 which
added a param to the fortis.watchlist insertion call without adding a
placeholder in the CQL.

Additionally, the topic category is required to always be non-null so we
default it to an empty string if the category is not set in the blob
watchlist terms.

src/resolvers-cassandra/Settings/mutations.js
@@ -29,8 +29,8 @@ function _insertTopics(siteType) {
     .then(response => {
       return response.map(topic => ({
         query: `INSERT INTO fortis.watchlist (topicid,topic,lang_code,translations,insertiontime,category) 
-                VALUES (?, ?, ?, ?, toTimestamp(now()));`,
-        params: [uuid(), topic.topic, topic.lang_code, topic.translations, topic.category]
+                VALUES (?, ?, ?, ?, toTimestamp(now()), ?);`,
+        params: [uuid(), topic.topic, topic.lang_code, topic.translations, topic.category || '']
       }));
     })
     .then(response => {