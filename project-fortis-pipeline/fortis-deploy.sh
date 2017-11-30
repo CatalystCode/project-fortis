864cda31cf7a6dad372c356b5ea87ecf797345d3
Clemens Wolff <clewolff@microsoft.com>
2017-11-29 22:59:05 -0500

    Fix formatting

--- project-fortis-pipeline/fortis-deploy.sh
+++ project-fortis-pipeline/fortis-deploy.sh
@@ -243,7 +243,10 @@ echo "Installed Helm. Adding storage share for spark checkpointing."
 echo "creating azure file share"
 readonly checkpointfileshare="checkpoint"
 
-az storage share create --name ${checkpointfileshare} --account-key="${storage_account_key}" --account-name="${storage_account_name}"
+az storage share create \
+    --name "${checkpointfileshare}" \
+    --account-key "${storage_account_key}" \
+    --account-name "${storage_account_name}"
 
 sleep 10
 
