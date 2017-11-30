7d3740b5478cbbe26d1840962a6fa71e8e7aae19
Clemens Wolff <clewolff@microsoft.com>
2017-11-29 23:32:44 -0500

    Add missing index.html reference

--- project-fortis-pipeline/ops/create-tags.sh
+++ project-fortis-pipeline/ops/create-tags.sh
@@ -5,6 +5,6 @@ readonly fortis_interface_host="${2}"
 readonly site_name="${3}"
 readonly graphql_service_host="${4}"
 
-az group update --name "${k8resource_group}" --set tags.FORTIS_INTERFACE_URL="${fortis_interface_host}/#/site/${site_name}"
-az group update --name "${k8resource_group}" --set tags.FORTIS_ADMIN_INTERFACE_URL="${fortis_interface_host}/#/site/${site_name}/admin"
+az group update --name "${k8resource_group}" --set tags.FORTIS_INTERFACE_URL="${fortis_interface_host}/index.html#/site/${site_name}"
+az group update --name "${k8resource_group}" --set tags.FORTIS_ADMIN_INTERFACE_URL="${fortis_interface_host}/index.html#/site/${site_name}/admin"
 az group update --name "${k8resource_group}" --set tags.FORTIS_SERVICE_HOST="${graphql_service_host}"
