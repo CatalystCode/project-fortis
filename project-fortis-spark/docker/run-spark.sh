5dca1eefe94e852cceacd397034106f5a4eaab73
Clemens Wolff <clewolff@microsoft.com>
2017-12-07 12:49:27 -0500

    Wait for cassandra schema to be defined

--- project-fortis-spark/docker/run-spark.sh
+++ project-fortis-spark/docker/run-spark.sh
@@ -15,5 +15,12 @@ while ! cassandra_exec; do
 done
 echo "...done, Cassandra is now available"
 
+# wait for cassandra schema to be defined
+while ! echo 'select * from fortis.sitesettings;' | cassandra_exec | grep -q '(1 rows)'; do
+  echo "Cassandra schema is not yet complete, waiting..."
+  sleep 10s
+done
+echo "...done, Cassandra schema is now complete"
+
 readonly spark_jar="$(find /app/target -name '*-assembly-0.0.0.jar' -exec readlink -f {} \; -quit)"
 spark-submit --driver-memory "${SPARK_DRIVER_MEMORY}" --class "${SPARK_MAINCLASS}" "${spark_jar}"
