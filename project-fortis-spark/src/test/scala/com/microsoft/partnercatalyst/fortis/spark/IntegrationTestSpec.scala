1c96e7e4cd4c1f85909b3b7773837ec591d44832
Clemens Wolff <clewolff@microsoft.com>
2017-12-11 14:17:01 -0500

    Guard against set but empty environment variables

--- project-fortis-spark/src/test/scala/com/microsoft/partnercatalyst/fortis/spark/IntegrationTestSpec.scala
+++ project-fortis-spark/src/test/scala/com/microsoft/partnercatalyst/fortis/spark/IntegrationTestSpec.scala
@@ -7,7 +7,10 @@ import scala.util.Properties.{envOrElse, envOrNone}
 class IntegrationTestSpec extends FlatSpec {
   protected def checkIfShouldRunWithLocalModels(): Option[String] = {
     val runIntegrationTests = envOrElse("FORTIS_INTEGRATION_TESTS", "false").toBoolean
-    val localModels = envOrNone("FORTIS_MODELS_DIRECTORY")
+    var localModels = envOrNone("FORTIS_MODELS_DIRECTORY")
+    if (localModels.getOrElse("").equals("")) {
+      localModels = None
+    }
     if (!runIntegrationTests && localModels.isEmpty) {
       cancel("Integration tests disabled and no local models available")
     }
