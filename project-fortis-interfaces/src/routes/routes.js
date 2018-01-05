772bb61fe8e9bce9e982696cf62a0ff2e95e31bd
Clemens Wolff <clewolff@microsoft.com>
2018-01-05 14:53:23 -0500

    Fix spacing

--- project-fortis-interfaces/src/routes/routes.js
+++ project-fortis-interfaces/src/routes/routes.js
@@ -1,16 +1,16 @@
 import { Route } from 'react-router'
-import {AppPage} from './AppPage';
-import {EntryPage} from './EntryPage';
-import {FactsPage} from './FactsPage';
-import {AdminPage} from './AdminPage';
-import {NotFoundPage} from './NotFoundPage';
+import { AppPage } from './AppPage';
+import { EntryPage } from './EntryPage';
+import { FactsPage } from './FactsPage';
+import { AdminPage } from './AdminPage';
+import { NotFoundPage } from './NotFoundPage';
 import React from 'react';
 
 export const routes = (
-    <Route path="/" component={AppPage} linkLabel="My App" icon="fa fa-share-alt-square fa">
-        <Route path="site/:siteKey" component={EntryPage} />
-        <Route path="site/:siteKey/facts" component={FactsPage} />
-        <Route path="site/:siteKey/admin" component={AdminPage} />
-        <Route path="*" component={NotFoundPage} />
-    </Route>
+  <Route path="/" component={AppPage} linkLabel="My App" icon="fa fa-share-alt-square fa">
+    <Route path="site/:siteKey" component={EntryPage} />
+    <Route path="site/:siteKey/facts" component={FactsPage} />
+    <Route path="site/:siteKey/admin" component={AdminPage} />
+    <Route path="*" component={NotFoundPage} />
+  </Route>
 );
