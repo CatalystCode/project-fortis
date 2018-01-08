be44e1d0cda6ef97d733ae959429dace7d806a86
Clemens Wolff <clewolff@microsoft.com>
2018-01-08 12:45:37 -0500

    Add default tile layer url

--- project-fortis-interfaces/src/config.js
+++ project-fortis-interfaces/src/config.js
@@ -1,4 +1,4 @@
-const reactAppMapboxTileLayerUrl = process.env.REACT_APP_MAPBOX_TILE_LAYER_URL;
+const reactAppMapboxTileLayerUrl = process.env.REACT_APP_MAPBOX_TILE_LAYER_URL || 'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}';
 const reactAppAdClientId = process.env.REACT_APP_AD_CLIENT_ID;
 const reactAppAdTokenStoreKey = process.env.REACT_APP_AD_TOKEN_STORE_KEY || 'Fortis.AD.Token';
 const reactAppServiceHost = process.env.REACT_APP_SERVICE_HOST;
