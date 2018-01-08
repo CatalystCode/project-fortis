243a2f1c3ade76132bfe8a06af733694edcd5c41
Clemens Wolff <clewolff@microsoft.com>
2018-01-08 12:48:18 -0500

    Error on unset required environment variables

--- project-fortis-interfaces/src/config.js
+++ project-fortis-interfaces/src/config.js
@@ -5,8 +5,10 @@ const reactAppAdTokenStoreKey = process.env.REACT_APP_AD_TOKEN_STORE_KEY || 'For
 const reactAppAdClientId = process.env.REACT_APP_AD_CLIENT_ID || '';
 
 const reactAppServiceHost = process.env.REACT_APP_SERVICE_HOST;
+if (!reactAppServiceHost) console.error('Service host is not defined!');
 
 const reactAppFeatureServiceHost = process.env.REACT_APP_FEATURE_SERVICE_HOST;
+if (!reactAppFeatureServiceHost) console.error('Feature service host is not defined!');
 
 module.exports = {
     reactAppMapboxTileLayerUrl,
