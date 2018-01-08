90a0b86fc0142aa88566d39bef222ea9cb5dc4ce
Clemens Wolff <clewolff@microsoft.com>
2018-01-08 12:47:19 -0500

    Formatting fixes (no functional change)

--- project-fortis-interfaces/src/config.js
+++ project-fortis-interfaces/src/config.js
@@ -1,7 +1,11 @@
 const reactAppMapboxTileLayerUrl = process.env.REACT_APP_MAPBOX_TILE_LAYER_URL || 'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}';
-const reactAppAdClientId = process.env.REACT_APP_AD_CLIENT_ID;
+
 const reactAppAdTokenStoreKey = process.env.REACT_APP_AD_TOKEN_STORE_KEY || 'Fortis.AD.Token';
+
+const reactAppAdClientId = process.env.REACT_APP_AD_CLIENT_ID || '';
+
 const reactAppServiceHost = process.env.REACT_APP_SERVICE_HOST;
+
 const reactAppFeatureServiceHost = process.env.REACT_APP_FEATURE_SERVICE_HOST;
 
 module.exports = {
