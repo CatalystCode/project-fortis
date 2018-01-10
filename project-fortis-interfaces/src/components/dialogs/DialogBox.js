59a3eec9b8c0128dba72f16323e8c2095a1a047c
Clemens Wolff <clewolff@microsoft.com>
2018-01-10 15:47:41 -0500

    Fix spacing

--- project-fortis-interfaces/src/components/dialogs/DialogBox.js
+++ project-fortis-interfaces/src/components/dialogs/DialogBox.js
@@ -6,70 +6,70 @@ import EventDetails from './EventDetails';
 import '../../styles/Insights/DialogBox.css';
 
 const dialogWideStyle = {
-    width: '80%',
-    height: '80%',
-    maxWidth: 'none'
+  width: '80%',
+  height: '80%',
+  maxWidth: 'none'
 };
 
 export default class DialogBox extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            open: false,
-            enabledStreams: props.enabledStreams
-        };
-    }
+    this.state = {
+      open: false,
+      enabledStreams: props.enabledStreams
+    };
+  }
 
-    open = (id) => {
-        this._loadDetail(id);
-    }
+  open = (id) => {
+    this._loadDetail(id);
+  }
 
-    close = () => {
-        this.setState({ open: false });
-    }
+  close = () => {
+    this.setState({ open: false });
+  }
 
-    _loadDetail(id){
-        SERVICES.FetchMessageDetail(id, (error, response, body) => {
-            if (error || response.statusCode !== 200 || !body.data || !body.data.event ) {
-                console.error("Failed to fetch details for id:", id, error);
-                return;
-            }
+  _loadDetail(id){
+    SERVICES.FetchMessageDetail(id, (error, response, body) => {
+      if (error || response.statusCode !== 200 || !body.data || !body.data.event ) {
+        console.error("Failed to fetch details for id:", id, error);
+        return;
+      }
 
-            let payload = Object.assign({}, body.data.event, {open: true});
-            this.setState({...payload});
-        });
-    }
+      let payload = Object.assign({}, body.data.event, {open: true});
+      this.setState({...payload});
+    });
+  }
 
-    render() {
-        const { open } = this.state;
-        const { language, settings } = this.props;
+  render() {
+    const { open } = this.state;
+    const { language, settings } = this.props;
 
-        const actions = [
-            <FlatButton
-                label="Done"
-                primary={true}
-                onTouchTap={this.close}
-            />,
-        ];
+    const actions = [
+      <FlatButton
+        label="Done"
+        primary={true}
+        onTouchTap={this.close}
+      />,
+    ];
 
-        return (
-            <Dialog
-                actions={actions}
-                modal={false}
-                open={open}
-                onRequestClose={this.close}
-                contentStyle={dialogWideStyle}
-                autoScrollBodyContent
-            >
-                <div className="content">
-                    <EventDetails
-                        {...this.state}
-                        pageLanguage={language}
-                        settings={settings}
-                    />
-                </div>
-            </Dialog>
-        );
-    }
+    return (
+      <Dialog
+        actions={actions}
+        modal={false}
+        open={open}
+        onRequestClose={this.close}
+        contentStyle={dialogWideStyle}
+        autoScrollBodyContent
+      >
+        <div className="content">
+          <EventDetails
+            {...this.state}
+            pageLanguage={language}
+            settings={settings}
+          />
+        </div>
+      </Dialog>
+    );
+  }
 };
