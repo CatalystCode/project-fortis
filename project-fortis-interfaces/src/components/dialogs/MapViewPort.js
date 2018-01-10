97bfbfa524cc67cb41ac44cf54d4e17bdfb2b258
Clemens Wolff <clewolff@microsoft.com>
2018-01-10 14:15:07 -0500

    Move tile server url to configuration

--- project-fortis-interfaces/src/components/dialogs/MapViewPort.js
+++ project-fortis-interfaces/src/components/dialogs/MapViewPort.js
@@ -2,10 +2,11 @@ import geoViewport from 'geo-viewport';
 import PropTypes from 'prop-types';
 import turfExtent from 'turf-extent';
 import React from 'react';
+import { reactAppMapboxTileServerUrl } from '../../config';
 
-const tileStyle = "mapbox.streets";
 const DEFAULT_ZOOM = 8;
-const tileServer = "https://api.mapbox.com/v4";
+const PIN_COLOR = '0000FF';
+const PIN_SIZE = 'l';
 
 class MapViewPort extends React.Component {
 render() {
@@ -19,13 +20,11 @@ render() {
         }
     ));
 
-    const color = '0000FF';
-    const pinsize = 'l';
     const geoJson = Object.assign({}, {"type": "FeatureCollection", "features": geoJsonFeatures});
     const bounds = turfExtent(geoJson);
     const vp = geoViewport.viewport(bounds, this.props.mapSize);
-    const pins = this.props.coordinates.map(coordinatePair => `pin-${pinsize}-cross+${color}(${coordinatePair.join(",")})`);
-    const mapImageSrc = `${tileServer}/${tileStyle}/${pins.join(',')}/${vp.center.join(',')},${pins.length > 1 ? vp.zoom : DEFAULT_ZOOM}@2x/${this.props.mapSize.join('x')}.png?access_token=${this.props.accessToken}`;
+    const pins = this.props.coordinates.map(coordinatePair => `pin-${PIN_SIZE}-cross+${PIN_COLOR}(${coordinatePair.join(",")})`);
+    const mapImageSrc = `${reactAppMapboxTileServerUrl}/${pins.join(',')}/${vp.center.join(',')},${pins.length > 1 ? vp.zoom : DEFAULT_ZOOM}@2x/${this.props.mapSize.join('x')}.png?access_token=${this.props.accessToken}`;
 
         return (
             <img src={mapImageSrc} alt="" width="100%"/>
--- project-fortis-interfaces/src/config.js
+++ project-fortis-interfaces/src/config.js
@@ -1,5 +1,7 @@
 const reactAppMapboxTileLayerUrl = process.env.REACT_APP_MAPBOX_TILE_LAYER_URL || 'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}';
 
+const reactAppMapboxTileServerUrl = process.env.REACT_APP_MAPBOX_TILE_SERVER_URL || 'https://api.mapbox.com/v4/mapbox.streets';
+
 const reactAppAdTokenStoreKey = process.env.REACT_APP_AD_TOKEN_STORE_KEY || 'Fortis.AD.Token';
 
 const reactAppAdClientId = process.env.REACT_APP_AD_CLIENT_ID || '';
@@ -12,6 +14,7 @@ if (!reactAppFeatureServiceHost) console.error('Feature service host is not defi
 
 module.exports = {
     reactAppMapboxTileLayerUrl,
+    reactAppMapboxTileServerUrl,
     reactAppAdClientId,
     reactAppAdTokenStoreKey,
     reactAppServiceHost,
