cb55b5cb786c07afdef910f1b0837e36a02e466d
Clemens Wolff <clewolff@microsoft.com>
2018-01-10 14:16:14 -0500

    Fix spacing

--- project-fortis-interfaces/src/components/dialogs/MapViewPort.js
+++ project-fortis-interfaces/src/components/dialogs/MapViewPort.js
@@ -8,16 +8,16 @@ const DEFAULT_ZOOM = 8;
 const PIN_COLOR = '0000FF';
 const PIN_SIZE = 'l';
 
-class MapViewPort extends React.Component {
-render() {
+export default class MapViewPort extends React.Component {
+  render() {
     const geoJsonFeatures = this.props.coordinates.map(coordinatePair => Object.assign({}, {
-            "type": "Feature",
-            "properties": {},
-            "geometry": {
-                "type": "Point",
-                "coordinates": coordinatePair
-            }
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Point",
+          "coordinates": coordinatePair
         }
+      }
     ));
 
     const geoJson = Object.assign({}, {"type": "FeatureCollection", "features": geoJsonFeatures});
@@ -26,15 +26,13 @@ render() {
     const pins = this.props.coordinates.map(coordinatePair => `pin-${PIN_SIZE}-cross+${PIN_COLOR}(${coordinatePair.join(",")})`);
     const mapImageSrc = `${reactAppMapboxTileServerUrl}/${pins.join(',')}/${vp.center.join(',')},${pins.length > 1 ? vp.zoom : DEFAULT_ZOOM}@2x/${this.props.mapSize.join('x')}.png?access_token=${this.props.accessToken}`;
 
-        return (
-            <img src={mapImageSrc} alt="" width="100%"/>
-        );
-    }
+    return (
+      <img src={mapImageSrc} alt="" width="100%"/>
+    );
+  }
 }
 
 MapViewPort.propTypes = {
-    coordinates: PropTypes.array.isRequired,
-    mapSize: PropTypes.array.isRequired
+  coordinates: PropTypes.array.isRequired,
+  mapSize: PropTypes.array.isRequired
 }
-
-export default MapViewPort;
