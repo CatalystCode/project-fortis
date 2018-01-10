7af97dacfc2e263da56d385fd097aec0a96091d4
Clemens Wolff <clewolff@microsoft.com>
2018-01-10 14:52:17 -0500

    Upgrade turf-extent to supported version

--- project-fortis-interfaces/src/components/dialogs/MapViewPort.js
+++ project-fortis-interfaces/src/components/dialogs/MapViewPort.js
@@ -1,6 +1,6 @@
 import geoViewport from 'geo-viewport';
 import PropTypes from 'prop-types';
-import turfExtent from 'turf-extent';
+import bbox from '@turf/bbox';
 import React from 'react';
 import { reactAppMapboxTileServerUrl } from '../../config';
 
@@ -21,7 +21,7 @@ export default class MapViewPort extends React.Component {
     ));
 
     const geoJson = Object.assign({}, {"type": "FeatureCollection", "features": geoJsonFeatures});
-    const bounds = turfExtent(geoJson);
+    const bounds = bbox(geoJson);
     const vp = geoViewport.viewport(bounds, this.props.mapSize);
     const pins = this.props.coordinates.map(coordinatePair => `pin-${PIN_SIZE}-cross+${PIN_COLOR}(${coordinatePair.join(",")})`);
     const mapImageSrc = `${reactAppMapboxTileServerUrl}/${pins.join(',')}/${vp.center.join(',')},${pins.length > 1 ? vp.zoom : DEFAULT_ZOOM}@2x/${this.props.mapSize.join('x')}.png?access_token=${this.props.accessToken}`;
