93e058dd9b6f6db1a6b50169ea363df72dfbb71a
Clemens Wolff <clewolff@microsoft.com>
2018-01-10 16:12:24 -0500

    Make term selector input style consistent

--- project-fortis-interfaces/src/components/Facts/FactsList.js
+++ project-fortis-interfaces/src/components/Facts/FactsList.js
@@ -84,6 +84,7 @@ export const FactsList = createReactClass({
             excludeLocations
             excludeSources
             allSiteTopics={this.props.fullTermList}
+            className="form-control edgeFilterInput"
             {...this.props}
           />
         </div>
