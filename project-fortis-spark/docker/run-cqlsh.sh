b3db33eb4437a6ed30a751616562f19b02e82bf9
Clemens Wolff <clewolff@microsoft.com>
2018-01-12 12:31:46 -0500

    Make spark cqlsh access consistent with services

---
+++ project-fortis-spark/docker/run-cqlsh.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+/opt/cassandra/bin/cqlsh \
+  --request-timeout=3600 \
+  --username="$FORTIS_CASSANDRA_USERNAME" \
+  --password="$FORTIS_CASSANDRA_PASSWORD" \
+  "$FORTIS_CASSANDRA_HOST"
--- project-fortis-spark/docker/run-spark.sh
+++ project-fortis-spark/docker/run-spark.sh
@@ -1,23 +1,15 @@
 #!/usr/bin/env bash
 
-cassandra_exec() {
-  /opt/cassandra/bin/cqlsh \
-    --request-timeout=3600 \
-    --username="$FORTIS_CASSANDRA_USERNAME" \
-    --password="$FORTIS_CASSANDRA_PASSWORD" \
-    "$FORTIS_CASSANDRA_HOST"
-}
-
 has_site() {
-  echo 'SELECT * FROM fortis.sitesettings;' | cassandra_exec | grep -q '(1 rows)'
+  echo 'SELECT * FROM fortis.sitesettings;' | /app/cqlsh | grep -q '(1 rows)'
 }
 
 get_tokens() {
-  echo 'COPY fortis.sitesettings(translationsvctoken,cogspeechsvctoken,cogvisionsvctoken,cogtextsvctoken) TO STDOUT;' | cassandra_exec | tr -dC '[A-Za-z0-9]'
+  echo 'COPY fortis.sitesettings(translationsvctoken,cogspeechsvctoken,cogvisionsvctoken,cogtextsvctoken) TO STDOUT;' | /app/cqlsh | tr -dC '[A-Za-z0-9]'
 }
 
 # wait for cassandra to start
-while ! cassandra_exec; do
+while ! /app/cqlsh; do
   echo "Cassandra not yet available, waiting..."
   sleep 10s
 done
