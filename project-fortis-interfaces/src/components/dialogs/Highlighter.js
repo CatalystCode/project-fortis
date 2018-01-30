0dadab5524bc9eb05b4ea29fdafa1cc657a6b265
Clemens Wolff <clewolff@microsoft.com>
2018-01-30 13:38:16 -0500

    Fix spacing

--- project-fortis-interfaces/src/components/dialogs/Highlighter.js
+++ project-fortis-interfaces/src/components/dialogs/Highlighter.js
@@ -2,42 +2,42 @@ import React from 'react';
 import { sanitize } from '../../utils/HtmlSanitizer';
 
 export class Highlighter extends React.Component {
-    sanitizeHtml(html) {
-        const { extraClasses } = this.props;
+  sanitizeHtml(html) {
+    const { extraClasses } = this.props;
 
-        html = sanitize(html, node => {
-            const classesToAdd = extraClasses[node.nodeName.toLowerCase()] || [];
-            classesToAdd.forEach(className => {
-                node.classList += className;
-            });
-            return node;
-        });
+    html = sanitize(html, node => {
+      const classesToAdd = extraClasses[node.nodeName.toLowerCase()] || [];
+      classesToAdd.forEach(className => {
+        node.classList += className;
+      });
+      return node;
+    });
 
-        return html;
-    }
+    return html;
+  }
 
-    highlightHtml(html) {
-        const { highlightWords } = this.props;
+  highlightHtml(html) {
+    const { highlightWords } = this.props;
 
-        const toHighlight = highlightWords.slice().sort((a, b) => a.word.length < b.word.length);
+    const toHighlight = highlightWords.slice().sort((a, b) => a.word.length < b.word.length);
 
-        toHighlight.forEach(({ word, className }) => {
-            html = html.replace(new RegExp(word, 'ig'), `<mark class="${className}">${word}</mark>`);
-        });
+    toHighlight.forEach(({ word, className }) => {
+      html = html.replace(new RegExp(word, 'ig'), `<mark class="${className}">${word}</mark>`);
+    });
 
-        return html;
-    }
+    return html;
+  }
 
-    renderHtml() {
-        let html = this.props.textToHighlight;
-        html = this.sanitizeHtml(html);
-        html = this.highlightHtml(html);
-        return html;
-    }
+  renderHtml() {
+    let html = this.props.textToHighlight;
+    html = this.sanitizeHtml(html);
+    html = this.highlightHtml(html);
+    return html;
+  }
 
-    render() {
-        return (
-            <span dangerouslySetInnerHTML={{__html: this.renderHtml()}} />
-        );
-    }
+  render() {
+    return (
+      <span dangerouslySetInnerHTML={{__html: this.renderHtml()}} />
+    );
+  }
 }
