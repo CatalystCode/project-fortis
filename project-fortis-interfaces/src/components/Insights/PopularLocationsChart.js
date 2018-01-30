118415457a999665e73170bde808745c7b014764
Clemens Wolff <clewolff@microsoft.com>
2018-01-30 11:57:51 -0500

    Fix spacing
    No functional change.

--- project-fortis-interfaces/src/components/Insights/PopularLocationsChart.js
+++ project-fortis-interfaces/src/components/Insights/PopularLocationsChart.js
@@ -6,72 +6,75 @@ import constants from '../../actions/constants';
 import { hasChanged } from './shared';
 
 export default class PopularLocationsChart extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            activeIndex: -1,
-            dataProvider: [],
-            selectedWofId: null,
-            colorCells: []
-        };
-    }
+    this.state = {
+      activeIndex: -1,
+      dataProvider: [],
+      selectedWofId: null,
+      colorCells: []
+    };
+  }
 
-    handleClick = (data, activeIndex) => {
-        const { placeid, centroid, bbox, name } = data;
+  handleClick = (data, activeIndex) => {
+    const { placeid, centroid, bbox, name } = data;
+    const { dataSource, timespanType, termFilters, defaultZoom, datetimeSelection, maintopic, externalsourceid, fromDate, toDate } = this.props;
 
-        const { dataSource, timespanType, termFilters, defaultZoom, datetimeSelection, maintopic, externalsourceid, fromDate, toDate } = this.props;
-        const place = { placeid: placeid, placecentroid: centroid, name: name, placebbox: bbox };
-        this.props.flux.actions.DASHBOARD.reloadVisualizationState(fromDate, toDate, datetimeSelection, timespanType, dataSource, maintopic,
-            bbox, defaultZoom, Array.from(termFilters), externalsourceid, null, place);
+    const place = { placeid: placeid, placecentroid: centroid, name: name, placebbox: bbox };
 
-        this.setState({ activeIndex: activeIndex, selectedWofId: placeid });
-    }
+    this.props.flux.actions.DASHBOARD.reloadVisualizationState(fromDate, toDate, datetimeSelection, timespanType, dataSource, maintopic, bbox, defaultZoom, Array.from(termFilters), externalsourceid, null, place);
 
-    refreshChart(props) {
-        const { popularLocations } = props;
-        let activeIndex = -1;
-        let { selectedWofId } = this.state;
-        let colorCells = [], dataProvider = [];
+    this.setState({ activeIndex: activeIndex, selectedWofId: placeid });
+  }
 
-        popularLocations.forEach((location, index) => {
-            const { name, mentions, bbox, placeid, centroid } = location;
-            const value = mentions;
-            if (placeid === selectedWofId) {
-                activeIndex = index;
-            }
-            let color = constants.CHART_STYLE.COLORS[index];
-            colorCells.push(<Cell key={0} fill={color} />);
+  refreshChart(props) {
+    const { popularLocations } = props;
+    const { selectedWofId } = this.state;
+    const colorCells = [];
+    const dataProvider = [];
+    let activeIndex = -1;
 
-            dataProvider.push(Object.assign({}, { value, name, bbox, placeid, centroid }));
-        });
-
-        this.setState({ colorCells, dataProvider, activeIndex });
-    }
-
-    componentDidMount() {
-        this.refreshChart(this.props);
-    }
-
-    componentWillReceiveProps(nextProps) {
-      if(hasChanged(this.props, nextProps)) {
-        this.refreshChart(nextProps);
+    popularLocations.forEach((location, index) => {
+      const { name, mentions, bbox, placeid, centroid } = location;
+      const value = mentions;
+      if (placeid === selectedWofId) {
+        activeIndex = index;
       }
+      const color = constants.CHART_STYLE.COLORS[index];
+      colorCells.push(<Cell key={0} fill={color} />);
+
+      dataProvider.push(Object.assign({}, { value, name, bbox, placeid, centroid }));
+    });
+
+    this.setState({ colorCells, dataProvider, activeIndex });
+  }
+
+  componentDidMount() {
+    this.refreshChart(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (hasChanged(this.props, nextProps)) {
+      this.refreshChart(nextProps);
+    }
+  }
+
+  render() {
+    if (!this.state.colorCells || !this.state.colorCells.length) {
+      return <NoData />;
     }
 
-    render() {
-        if (!this.state.colorCells || !this.state.colorCells.length) {
-            return <NoData />;
-        }
-
-        return (
-            <DoughnutChart handleClick={this.handleClick}
-                fill={constants.CHART_STYLE.BG_FILL}
-                language={this.props.language}
-                data={this.state.dataProvider}
-                activeIndex={this.state.activeIndex}>
-                {this.state.colorCells}
-            </DoughnutChart>
-        );
-    }
+    return (
+      <DoughnutChart
+        handleClick={this.handleClick}
+        fill={constants.CHART_STYLE.BG_FILL}
+        language={this.props.language}
+        data={this.state.dataProvider}
+        activeIndex={this.state.activeIndex}
+      >
+        {this.state.colorCells}
+      </DoughnutChart>
+    );
+  }
 }
