eb3e020d6f8f5d913f0016b0fecdad720fe56016
Clemens Wolff <clewolff@microsoft.com>
2018-01-30 15:21:33 -0500

    Make change language menu always fit on screen

--- project-fortis-interfaces/src/components/Insights/LanguagePicker.js
+++ project-fortis-interfaces/src/components/Insights/LanguagePicker.js
@@ -1,37 +1,60 @@
 import React from 'react';
+import Drawer from 'material-ui/Drawer';
 import ActionLanguage from 'material-ui/svg-icons/action/language';
 import IconButton from 'material-ui/IconButton';
-import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import { fullWhite } from 'material-ui/styles/colors';
 
 export default class LanguagePicker extends React.Component {
-  changeLanguage = (event, value) => {
-    this.props.onChangeLanguage(value);
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  changeLanguage = (language) => {
+    this.props.onChangeLanguage(language);
+    this.setState({ open: false });
+  }
+
+  handleDrawerToggle = () => {
+    this.setState({ open: !this.state.open });
+  }
+
+  handleDrawerChange = (open) => {
+    this.setState({ open });
   }
 
   renderMenuItems() {
-    return this.props.supportedLanguages.map(lang =>
-      <MenuItem key={lang} value={lang} primaryText={lang} label={`Language: ${lang}`} />
-    );
-  }
-
-  renderMenuIcon() {
-    const { language, tooltipPosition } = this.props;
-    const tooltip = `Current language: '${language}'. Click to change language`;
-
-    return (
-      <IconButton tooltip={tooltip} tooltipPosition={tooltipPosition}>
-        <ActionLanguage color={fullWhite} />
-      </IconButton>
+    return this.props.supportedLanguages.map(language =>
+      <MenuItem
+        key={language}
+        value={language}
+        primaryText={`Set language to ${language}`}
+        label={`Language: ${language}`}
+        onClick={() => this.changeLanguage(language)}
+      />
     );
   }
 
   render() {
+    const { language, tooltipPosition } = this.props;
+    const tooltip = `Current language: '${language}'. Click to change language`;
+
     return (
-      <IconMenu onChange={this.changeLanguage} iconButtonElement={this.renderMenuIcon()}>
-        {this.renderMenuItems()}
-      </IconMenu>
+      <div>
+        <IconButton tooltip={tooltip} tooltipPosition={tooltipPosition} onClick={this.handleDrawerToggle}>
+          <ActionLanguage color={fullWhite} />
+        </IconButton>
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={this.handleDrawerChange}>
+            {this.renderMenuItems()}
+        </Drawer>
+      </div>
     );
   }
 }
