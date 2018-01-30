6f42a3e53f288502791894faf49237a5ea813e4c
Clemens Wolff <clewolff@microsoft.com>
2018-01-30 16:42:15 -0500

    Simplify control flow
    No functional change.

--- project-fortis-interfaces/src/components/Graphics/Sentiment.js
+++ project-fortis-interfaces/src/components/Graphics/Sentiment.js
@@ -2,30 +2,27 @@ import React from 'react';
 import { getSentimentAttributes } from '../Insights/shared';
 
 export default class Sentiment extends React.Component {
-
   render() {
-    const { value } = this.props;
+    const { value, showGraph } = this.props;
     const sentiment = getSentimentAttributes(value);
     const className = `material-icons sentimentIcon ${sentiment.style}`;
     const sentimentIcon = <span className={className}>{sentiment.icon}</span>;
-    const displayValue = parseFloat(this.props.value * 10).toFixed(0);
+    const displayValue = parseFloat(value * 10).toFixed(0);
     const graphbarClassname = `sentimentGraphBar ${sentiment.style}`;
+
+    if (!showGraph) {
+      return <div>{sentimentIcon}</div>;
+    }
 
     return (
       <div>
-        {
-          this.props.showGraph ?
-            <div className="sentimentGraph">
-              <div className={graphbarClassname}>
-                  { displayValue }
-              </div>
-              {sentimentIcon}
-            </div>
-            : {sentimentIcon}
-        }
+        <div className="sentimentGraph">
+          <div className={graphbarClassname}>
+            { displayValue }
+          </div>
+          {sentimentIcon}
+        </div>
       </div>
-
     );
   }
-
 };
