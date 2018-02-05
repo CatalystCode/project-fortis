7a395eb0662aaedd036aefae9b1ae8617e2d4ec7
Clemens Wolff <clewolff@microsoft.com>
2018-02-05 10:23:46 -0500

    Fix spacing
    No functional change.

--- project-fortis-interfaces/src/services/shared.js
+++ project-fortis-interfaces/src/services/shared.js
@@ -4,20 +4,20 @@ import { reactAppServiceHost, reactAppAdTokenStoreKey } from '../config';
 const auth = { token: null }; // token will get set as soon as it's available
 
 function fetchGqlData(endpoint, { query, variables }, callback) {
-    request({
-        url: `${reactAppServiceHost}/api/${endpoint}`,
-        method: 'POST',
-        json: true,
-        withCredentials: false,
-        headers: { 'Authorization': `Bearer ${auth.token}` },
-        body: { query, variables }
-    }, (error, response, body) => {
-        if (response && response.statusCode === 401) {
-            auth.token = null;
-            localStorage.removeItem(reactAppAdTokenStoreKey);
-        }
-        callback(error, response, body);
-    });
+  request({
+    url: `${reactAppServiceHost}/api/${endpoint}`,
+    method: 'POST',
+    json: true,
+    withCredentials: false,
+    headers: { 'Authorization': `Bearer ${auth.token}` },
+    body: { query, variables }
+  }, (error, response, body) => {
+    if (response && response.statusCode === 401) {
+      auth.token = null;
+      localStorage.removeItem(reactAppAdTokenStoreKey);
+    }
+    callback(error, response, body);
+  });
 }
 
 const MESSAGES_ENDPOINT = 'messages';
@@ -26,10 +26,10 @@ const EDGES_ENDPOINT = 'edges';
 const SETTINGS_ENDPOINT = 'settings';
 
 module.exports = {
-    MESSAGES_ENDPOINT,
-    TILES_ENDPOINT,
-    EDGES_ENDPOINT,
-    SETTINGS_ENDPOINT,
-    auth,
-    fetchGqlData
+  MESSAGES_ENDPOINT,
+  TILES_ENDPOINT,
+  EDGES_ENDPOINT,
+  SETTINGS_ENDPOINT,
+  auth,
+  fetchGqlData
 };
