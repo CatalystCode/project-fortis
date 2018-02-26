3fc17614a5ec965cb75f33580c6b894b5780e066
Clemens Wolff <clewolff@microsoft.com>
2018-02-26 14:22:13 -0500

    Surface events and dependencies in local logs

--- project-fortis-spark/src/main/scala/com/microsoft/partnercatalyst/fortis/spark/logging/AppInsightsTelemetry.scala
+++ project-fortis-spark/src/main/scala/com/microsoft/partnercatalyst/fortis/spark/logging/AppInsightsTelemetry.scala
@@ -40,6 +40,8 @@ class AppInsightsTelemetry extends FortisTelemetry {
 
   def logEvent(name: String, properties: Map[String, String]=Map(), metrics: Map[String, Double]=Map()): Unit = {
     try {
+      System.err.println(s"Event $name|Properties ${properties.map(kv => s"${kv._1}=${kv._2}").mkString(",")}|Metrics ${metrics.map(kv => s"${kv._1}=${kv._2}").mkString(",")}}")
+
       val appInsightsProperties = new util.HashMap[String, String](properties.size)
       properties.foreach(kv => appInsightsProperties.put(kv._1, kv._2))
 
@@ -57,6 +59,8 @@ class AppInsightsTelemetry extends FortisTelemetry {
 
   def logDependency(name: String, method: String, success: Boolean, durationInMs: Long): Unit = {
     try {
+      System.err.println(s"Dependency $name|Method $method|Success $success|DurationInMs $durationInMs")
+
       client.trackDependency(name, method, new Duration(durationInMs), success)
       client.flush()
     } catch {
