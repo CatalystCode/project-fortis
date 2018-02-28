19a6a2cef754373f4921635f052caeeb7eac4924
Clemens Wolff <clewolff@microsoft.com>
2018-02-28 09:08:07 -0500

    Fix mismatch between spark and services

--- project-fortis-spark/src/main/scala/com/microsoft/partnercatalyst/fortis/spark/transformcontext/TransformContextMessages.scala
+++ project-fortis-spark/src/main/scala/com/microsoft/partnercatalyst/fortis/spark/transformcontext/TransformContextMessages.scala
@@ -1,9 +1,10 @@
 package com.microsoft.partnercatalyst.fortis.spark.transformcontext
 
+/** @note These have to be kept in sync with StreamingController.js in project-fortis-services **/
 object TransformContextMessages {
   val ChangeRequired = "dirty"
 
-  val SettingsChanged = "settings"
+  val SettingsChanged = "sitesettings"
   val WatchlistChanged = "watchlist"
   val BlacklistChanged = "blacklist"
 }
