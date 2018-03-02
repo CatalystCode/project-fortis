9074615e3fc4128b8ccc978e64f06ac83a5d1ada
Clemens Wolff <clewolff@microsoft.com>
2018-03-02 10:27:11 -0500

    Turn up logging for auth check

--- project-fortis-services/src/auth.js
+++ project-fortis-services/src/auth.js
@@ -88,7 +88,7 @@ function requiresRole(promiseFunc, requiredRole) {
 
       checkIfUserHasRoleCached(user, requiredRole)
         .then(hasRole => {
-          if (!hasRole) return reject('Unknown user');
+          if (!hasRole) return reject(`Unknown user ${user} for role ${requiredRole}`);
           return promiseFunc(...args)
             .then(resolve)
             .catch(reject);
